58b641c3877e8066c512d251399e8c036ae88cb2
Sean Massung <massung1@illinois.edu>
2013-01-19 20:23:00 -0600

    work on compressed file reader tests

--- test/compressed_file_test.h
+++ test/compressed_file_test.h
@@ -5,7 +5,11 @@
 #ifndef _COMPRESSED_FILE_H_
 #define _COMPRESSED_FILE_H_
 
+#include <map>
+#include <unordered_map>
 #include "unit_test.h"
+#include "util/invertible_map.h"
+#include "io/textfile.h"
 #include "io/compressed_file_reader.h"
 #include "io/compressed_file_writer.h"
 
@@ -13,13 +17,87 @@ namespace Tests
 {
     namespace CompressedFileTests
     {
+        using std::unordered_map;
+        using std::multimap;
+
+        unordered_map<char, size_t> freqs;
+        InvertibleMap<char, unsigned int> mapping;
+        string inputFilename;
+        string compressedFilename;
+        string uncompressedFilename;
+
+        /**
+         * Helper function; counts frequencies of a given file to determine best
+         * compression mapping.
+         * @param filename The file to analyze
+         * @return the mapping of char to count
+         */
+        unordered_map<char, size_t> getFreqs(const string & filename)
+        {
+            unordered_map<char, size_t> freqs;
+            TextFile textfile(filename);
+
+            char* start = textfile.opentext();
+            unsigned int index = 0;
+
+            while(index < textfile.get_size())
+                ++freqs[start[index++]];
+
+            textfile.closetext();
+            return freqs;
+        }
+
+        /**
+         * Creates a char to id mapping for the delta compression based on
+         * character counts.
+         * @param freqs The frequency data of the file to be compressed
+         * @return the mapping of char to id
+         */
+        InvertibleMap<char, unsigned int> getMapping(const unordered_map<char, size_t> & freqs)
+        {
+            multimap<size_t, char> sorted;
+            for(auto & it: freqs)
+                sorted.insert(make_pair(it.second, it.first));
+
+            InvertibleMap<char, unsigned int> mapping;
+            unsigned int value = 1;
+            for(auto it = sorted.rbegin(); it != sorted.rend(); ++it)
+            {
+                mapping.insert(it->second, value);
+                ++value;
+            }
+
+            return mapping;
+        }
+
         void testRead()
         {
+            CompressedFileReader reader(compressedFilename);
+            ofstream writer(uncompressedFilename);
+            while(reader.hasNext())
+            {
+                unsigned int val = reader.next();
+                writer << mapping.getKeyByValue(val);
+            }
+            writer.close();
             PASS;
         }
 
         void testWrite()
         {
+            TextFile textfile(inputFilename);
+            CompressedFileWriter writer(compressedFilename);
+
+            char* start = textfile.opentext();
+            unsigned int index = 0;
+
+            while(index < textfile.get_size())
+            {
+                unsigned int toWrite = mapping.getValueByKey(start[index++]);
+                writer.write(toWrite);
+            }
+
+            textfile.closetext();
             PASS;
         }
 
@@ -30,6 +108,16 @@ namespace Tests
 
         void isSmaller()
         {
+            ASSERT(1 == 1);
+        }
+
+        void init()
+        {
+            inputFilename = "data/to_compress.txt";
+            compressedFilename = "data/compressed.txt";
+            uncompressedFilename = "data/uncompressed.txt";
+            freqs = getFreqs(string(inputFilename));
+            mapping = getMapping(freqs);
             PASS;
         }
     }
--- test/tester.cpp
+++ test/tester.cpp
@@ -14,135 +14,6 @@ using std::cout;
 using std::endl;
 using namespace UnitTests;
 
-/*
-unordered_map<char, size_t> getFreqs(string filename)
-{
-    unordered_map<char, size_t> freqs;
-    TextFile textfile(filename);
-
-    char* start = textfile.opentext();
-    unsigned int length = textfile.get_size();
-    unsigned int index = 0;
-
-    while(index < length)
-    {
-        ++freqs[start[index]];
-        if(++index % 1000 == 0)
-            cerr << "  " << (double) index / length * 100 << "%    \r";
-    }
-    cerr << "  100.00%  " << endl;
-
-    textfile.closetext();
-    return freqs;
-}
-
-void compress(string filename, string outfilename, const InvertibleMap<char, unsigned int> & mapping)
-{
-    TextFile textfile(filename);
-    CompressedFileWriter writer(outfilename);
-
-    char* start = textfile.opentext();
-    unsigned int length = textfile.get_size();
-    unsigned int index = 0;
-
-    while(index < length)
-    {
-        unsigned int toWrite = mapping.getValueByKey(start[index]);
-        writer.write(toWrite);
-        if(++index % 1000 == 0)
-            cerr << "  " << (double) index / length * 100 << "%    \r";
-    }
-    cerr << "  100.00%  " << endl;
-
-    textfile.closetext();
-}
-
-void decompress(string infilename, string outfilename, const InvertibleMap<char, unsigned int> & mapping)
-{
-    CompressedFileReader reader(infilename);
-    ofstream writer(outfilename);
-    unsigned int val;
-    while(reader.hasNext())
-    {
-        val = reader.next();
-        writer << mapping.getKeyByValue(val);
-    }
-    writer.close();
-}
-
-InvertibleMap<char, unsigned int> getMapping(const unordered_map<char, size_t> & freqs)
-{
-    multimap<size_t, char> sorted;
-    for(auto & it: freqs)
-        sorted.insert(make_pair(it.second, it.first));
-
-    InvertibleMap<char, unsigned int> mapping;
-    unsigned int value = 1;
-    for(auto it = sorted.rbegin(); it != sorted.rend(); ++it)
-    {
-        mapping.insert(it->second, value);
-        ++value;
-    }
-
-    return mapping;
-}
-
-void testCompression(string filename)
-{
-    cerr << "Getting frequencies..." << endl;
-    double start = omp_get_wtime();
-    unordered_map<char, size_t> freqs = getFreqs(string(filename));
-    cerr << "  found " << freqs.size() << " unique characters" << endl;
-    cerr << "  " << omp_get_wtime() - start << " seconds elapsed" << endl;
-
-    InvertibleMap<char, unsigned int> mapping = getMapping(freqs);
-
-    cerr << "Compressing..." << endl;
-    start = omp_get_wtime();
-    compress(string(filename), "compressed.txt", mapping);
-    cerr << "  " << omp_get_wtime() - start << " seconds elapsed" << endl;
-
-    cerr << "Decompressing..." << endl;
-    start = omp_get_wtime();
-    decompress("compressed.txt", "uncompressed.txt", mapping);
-    cerr << "  " << omp_get_wtime() - start << " seconds elapsed" << endl;
-}
-*/
-
-void thing()
-{
-    ASSERT(1 == 1);
-    ASSERT(2 == 3);
-    PASS;
-}
-
-void thing1()
-{
-    int* p = NULL;
-    *p = 1;
-    PASS;
-}
-
-void thing2()
-{
-    while(1);
-    PASS;
-}
-
-void thing3()
-{
-    PASS;
-}
-
-void testTest()
-{
-    cout << "Running tests..." << endl;
-    UnitTests::runTest("MyFirstTest", thing);
-    UnitTests::runTest("MySecondTest", thing1);
-    UnitTests::runTest("InfiniteLoopTest", thing2, 3);
-    UnitTests::runTest("LastTest", thing3);
-}
-
 void porter2Stemmer()
 {
     using namespace Tests::Porter2StemmerTests;
@@ -172,8 +43,9 @@ void compressedFile()
     using namespace Tests::CompressedFileTests;
 
     cout << Common::makeBold("[compressed_file]") << endl;
-    runTest("read", testRead);
+    runTest("init", init);
     runTest("write", testWrite);
+    runTest("read", testRead);
     runTest("correct", correct);
     runTest("isSmaller", isSmaller);
 }
