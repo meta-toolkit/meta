c42861d7a7b738ca1b1cf5c1e45ad2a1e61f3f12
Sean Massung <massung1@illinois.edu>
2013-02-09 14:14:33 -0600

    finished confusion matrix -> kappa score script

--- eval/eval.rb
+++ eval/eval.rb
@@ -11,15 +11,15 @@ end
 
 def runTest(config)
   createConfigFile(config)
-  liblinear = "lib/liblinear-1.92/train"
+  liblinear = "../lib/liblinear-1.92/train"
   svmMethod = "-s 2"
   opts = "-v 5 -q"
-  `./learn evalConfig.ini > evalOutput`
+  `../learn evalConfig.ini > evalOutput`
   result = `#{liblinear} #{svmMethod} #{opts} evalOutput`
   filename = "#{config["prefix"]}-#{config["method"]}-#{config["ngramOpt"]}-#{config["ngram"]}-#{config["treeOpt"]}"
   filename.gsub!(/-[-]+/, "-")
   filename.gsub!(/-$/, "")
-  File.open("runs/#{filename}.run", "w") { |f| f.write(result) }
+  File.open("../runs/#{filename}.run", "w") { |f| f.write(result) }
   f1 = /^ f1:([0-9\.]+)/.match(result)[1].to_f
   acc = /^ f1:([0-9\.]+) acc:([0-9\.]+)/.match(result)[2].to_f
   puts "#{f1.round(4)}\t#{acc.round(4)}\t#{config}"
@@ -50,7 +50,7 @@ def main()
 
   #for method in ["Word", "POS", "FW", "Char"]
   for method in ["Char"]
-    for n in (4..6)
+    for n in (5..6)
       config["method"] = "both"
       config["ngramOpt"] = method
       config["ngram"] = n
--- eval/kaggle-eval.py
+++ eval/kaggle-eval.py
@@ -1,8 +1,25 @@
+import subprocess
 from score import *
 
 def main():
-  rater_a = [12, 3, 1, 4, 5, 7, 8]
-  rater_b = [12, 3, 1, 4, 5, 7, 2]
-  print(quadratic_weighted_kappa(rater_a, rater_b))
+  files = subprocess.check_output(["ls", "../runs"]).decode("utf-8").split()
+  files = [f for f in files if "kaggle" in f]
+  for f in files:
+    lines = open("../runs/" + f).readlines()
+    lines = lines[0:13] # only get confusion matrix part
+    rater_a, rater_b = [], []
+    pred = 0
+    try:
+      for line in lines:
+        line = line.replace("[", "")
+        line = line.replace("]", "")
+        counts = [int(c) for c in line.split()]
+        for actual in range(0, 13):
+          rater_a.extend([pred] * counts[actual]) # guessed pred actual times
+          rater_b.extend([actual] * counts[actual]) # actual "actual" times
+        pred += 1
+      print(str(quadratic_weighted_kappa(rater_a, rater_b)) + " " + f)
+    except:
+      print("error on " + f)
 
 main()
