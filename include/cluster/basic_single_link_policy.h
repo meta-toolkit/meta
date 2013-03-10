98c3efb05365328135faf9776a0cf9cb008a4cf3
Chase Geigle <sky@skystrife.com>
2013-03-09 23:43:57 -0600

    Add multithreading to basic_single_link_policy with the thread_pool class.

--- include/cluster/basic_single_link_policy.h
+++ include/cluster/basic_single_link_policy.h
@@ -12,6 +12,7 @@
 #include <utility>
 
 #include "cluster/point.h"
+#include "parallel/thread_pool.h"
 
 namespace clustering {
     
@@ -28,44 +29,60 @@ class basic_single_link_policy {
     public:
         template <class RootList>
         void merge_clusters( RootList & current_roots ) {
+            // current_roots is a list of unique_ptr to treenodes
             std::cout << "merge_clusters()" << std::endl;
 
             using treeptr = typename RootList::value_type;
+            using iterator = typename RootList::iterator;
 
-            // current_roots is a list of unique_ptr to treenodes
-            double min_distance = std::numeric_limits<double>::max();
-            auto to_merge = std::make_pair( current_roots.begin(), 
-                                            current_roots.begin() );
-            size_t remaining_loops = current_roots.size();
+            parallel::thread_pool pool;
+
+            std::vector<std::future<std::tuple<size_t, iterator, iterator>>> futures;
             for( auto it = current_roots.begin(); 
                     it != current_roots.end(); 
                     ++it ) {
-                std::cout << "iterations remaining: " << (remaining_loops--) << "           \r" << std::flush;
-                auto it2 = it;
-                ++it2;
-                for( ; it2 != current_roots.end(); ++it2 ) {
-                    double distance = single_link_distance( *it, *it2 );
-                    if( distance < min_distance ) {
-                        min_distance = distance;
-                        to_merge = std::make_pair( it, it2 );
-                    }
-                }
+                futures.push_back(
+                        pool.submit_task( [&,it]() {
+                                auto result = std::make_tuple( std::numeric_limits<size_t>::max(), it, it );
+                                auto it2 = it;
+                                ++it2;
+                                for( ; it2 != current_roots.end(); ++it2 ) {
+                                    double distance = single_link_distance( *it, *it2 );
+                                    if( distance < std::get<0>( result ) ) {
+                                        std::get<0>( result ) = distance;
+                                        std::get<1>( result ) = it;
+                                        std::get<2>( result ) = it2;
+                                    }
+                                }
+                                return result;
+                            }
+                        )
+                    );
+            }
+            
+            auto best_merge = std::make_tuple( std::numeric_limits<size_t>::max(),
+                                               current_roots.begin(), 
+                                               current_roots.begin() );
+            for( auto & fut : futures ) {
+                auto tuple = fut.get();
+                if( std::get<0>( tuple ) < std::get<0>( best_merge ) )
+                    best_merge = tuple;
             }
 
             std::cout << std::endl << "finished merge_clusters()" << std::endl;
 
-            treeptr left = std::move( *to_merge.first );
-            treeptr right = std::move( *to_merge.second );
+            treeptr left = std::move( *std::get<1>( best_merge ) );
+            treeptr right = std::move( *std::get<2>( best_merge ) );
 
-            current_roots.erase( to_merge.first );
-            current_roots.erase( to_merge.second );
+            current_roots.erase( std::get<1>( best_merge ) );
+            current_roots.erase( std::get<2>( best_merge ) );
 
             using treenode = typename treeptr::element_type;
             current_roots.emplace_back( 
                 new treenode{ std::move( left ), std::move( right ) } );
         }
     private:
-        
+
         template <class TreeNodePtr>
         double single_link_distance( const TreeNodePtr & first, 
                                      const TreeNodePtr & second ) {
