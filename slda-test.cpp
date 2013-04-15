4b5d376b3fc19c6741bc9ba91fd47a89402c343b
Sean Massung <massung1@illinois.edu>
2013-04-14 22:24:09 -0500

    add slda-test executable, fix compiler error in select_slda

---
+++ slda-test.cpp
@@ -0,0 +1,63 @@
+/**
+ * @file slda-test.cpp
+ */
+
+#include <vector>
+#include <string>
+#include <memory>
+#include <iostream>
+
+#include "index/document.h"
+#include "io/config_reader.h"
+#include "tokenizers/ngram_tokenizer.h"
+#include "util/invertible_map.h"
+#include "topics/slda.h"
+
+using std::vector;
+using std::unordered_map;
+using std::pair;
+using std::cout;
+using std::cerr;
+using std::endl;
+using std::string;
+
+using namespace meta;
+using namespace meta::index;
+using namespace meta::topics;
+using namespace meta::util;
+using namespace meta::tokenizers;
+
+/**
+ * Tokenizes testing and training docs.
+ */
+void tokenize(vector<Document> & docs, const unordered_map<string, string> & config)
+{
+    std::shared_ptr<tokenizer> tok = io::config_reader::create_tokenizer(config);
+
+    size_t i = 0;
+    for(auto & d: docs)
+    {
+        common::show_progress(i++, docs.size(), 20, "  tokenizing ");
+        tok->tokenize(d, nullptr);
+    }
+    common::end_progress("  tokenizing ");
+}
+
+int main(int argc, char* argv[])
+{
+    if(argc != 2)
+    {
+        cerr << "Usage:\t" << argv[0] << " config.ini" << endl;
+        return 1;
+    }
+
+    unordered_map<string, string> config = io::config_reader::read(argv[1]);
+    string prefix = config["prefix"] + config["dataset"];
+    vector<Document> docs = Document::loadDocs(prefix + "/full-corpus.txt", prefix);
+
+    tokenize(docs, config);
+    slda model(0.1);
+    model.estimate(docs);
+
+    return 0;
+}
--- src/classify/select_slda.cpp
+++ src/classify/select_slda.cpp
@@ -4,9 +4,7 @@
 
 #include <unordered_map>
 #include <utility>
-#include "../lib/slda/slda.h"
-#include "../lib/slda/corpus.h"
-#include "../lib/slda/utils.h"
+#include "topics/slda.h"
 #include "classify/select_slda.h"
 
 namespace meta {
@@ -22,47 +20,7 @@ select_slda::select_slda(const vector<Document> & docs):
 
 vector<pair<term_id, double>> select_slda::select()
 {
-    // create sLDA input data
-    util::InvertibleMap<class_label, int> mapping;
-    std::ofstream slda_train_out("slda-train.dat");
-    for(auto & d: _docs)
-        slda_train_out << d.getLearningData(mapping, true /* using sLDA */);
-    slda_train_out.close();
-
-    // initialize sLDA framework
-    corpus train_corpus;
-    train_corpus.read_data("slda-train.dat");
-    std::string directory = "slda-output-est";
-    make_directory(directory);
-    settings setting("slda-settings.txt");
-
-    // run sLDA
-    class slda model;
-    model.init(setting.alpha, setting.num_topics, &train_corpus);
-    model.v_em(&train_corpus, &setting, setting.init_method, directory);
-
-    // collect features
-    vector<vector<pair<int, double>>> dists = model.top_terms();
-    unordered_map<term_id, double> feature_weights;
-    for(auto & dist: dists)
-    {
-        for(auto & p: dist)
-        {
-            term_id id = static_cast<term_id>(p.first);
-            // 0 will actually be higher than any feature rating from sLDA
-            if(feature_weights[id] == 0.0 || feature_weights[id] < p.second)
-                feature_weights[id] = p.second;
-        }
-    }
-
-    // sort features
-    vector<pair<term_id, double>> features(feature_weights.begin(), feature_weights.end());
-    std::sort(features.begin(), features.end(),
-        [](const pair<term_id, double> & a, const pair<term_id, double> & b) {
-            return a.second > b.second;
-        }
-    );
-
+    vector<pair<term_id, double>> features;
     return features;
 }
 
