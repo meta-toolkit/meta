e9c8d7a6f17fd806d3dd3da78a3c838bb119ebba
Chase Geigle <geigle1@illinois.edu>
2013-10-14 18:34:31 -0500

    Add parallelization to tokenization process for forward_index.

--- src/index/forward_index.cpp
+++ src/index/forward_index.cpp
@@ -23,37 +23,63 @@ forward_index::forward_index(const cpptoml::toml_group & config):
 uint32_t forward_index::tokenize_docs(
         const std::unique_ptr<corpus::corpus> & docs)
 {
-    std::vector<postings_data<doc_id, term_id>> pdata;
-    pdata.reserve(docs->size());
-    uint32_t chunk_num = 0;
+    std::atomic<uint32_t> chunk_num{0};
+    const uint32_t chunk_size = 500;
     std::string progress = "Tokenizing ";
-    while(docs->has_next())
-    {
-        corpus::document doc{docs->next()};
-        common::show_progress(doc.id(), docs->size(), 20, progress);
-        _tokenizer->tokenize(doc);
-        _doc_id_mapping.push_back(doc.path());
-        _doc_sizes.push_back(doc.length());
+    std::mutex mutex;
 
-        postings_data<doc_id, term_id> pd{doc.id()};
-        pd.set_counts(std::vector<std::pair<term_id, double>>{
-            doc.frequencies().begin(), doc.frequencies().end()
-        });
-        pdata.push_back(pd);
+    // allocate metadata
+    _doc_id_mapping.resize(docs->size());
+    _doc_sizes.resize(docs->size());
+    _unique_terms.resize(docs->size());
+    _labels.resize(docs->size());
 
-        // Save class label information
-        _unique_terms.push_back(doc.frequencies().size());
-        _labels.push_back(doc.label());
+    auto task = [&]() {
+        std::vector<postings_data<doc_id, term_id>> pdata;
+        pdata.reserve(chunk_size);
+        while (true) {
+            util::optional<corpus::document> doc;
+            {
+                std::lock_guard<std::mutex> lock{mutex};
+                if (docs->has_next()) {
+                    doc = docs->next();
+                    common::show_progress(doc->id(), docs->size(), 20, progress);
+                }
+            }
 
-        // every k documents, write a chunk
-        // TODO: make this based on memory usage instead
-        if(doc.id() % 500 == 0)
-            write_chunk(chunk_num++, pdata);
-    }
+            if (!doc) {
+                if (!pdata.empty())
+                    write_chunk(chunk_num.fetch_add(1), pdata);
+                return;
+            }
+
+            _tokenizer->tokenize(*doc);
+
+            _doc_id_mapping[doc->id()] = doc->path();
+            _doc_sizes[doc->id()] = doc->length();
+
+            postings_data<doc_id, term_id> pd{doc->id()};
+            pd.set_counts(std::vector<std::pair<term_id, double>>{
+                doc->frequencies().begin(), doc->frequencies().end()
+            });
+            pdata.push_back(pd);
+
+            _unique_terms[doc->id()] = doc->frequencies().size();
+            _labels[doc->id()] = doc->label();
+
+            if (pdata.size() % chunk_size == 0)
+                write_chunk(chunk_num.fetch_add(1), pdata);
+        }
+    };
+
+    parallel::thread_pool pool;
+    std::vector<std::future<void>> futures;
+    for (size_t i = 0; i < pool.thread_ids().size(); ++i)
+        futures.emplace_back(pool.submit_task(task));
+
+    for (auto & fut : futures)
+        fut.get();
     common::end_progress(progress);
-   
-    if(!pdata.empty())
-        write_chunk(chunk_num++, pdata);
 
     return chunk_num;
 }
@@ -74,7 +100,7 @@ std::string forward_index::liblinear_data(doc_id d_id) const
     sorted.reserve(pdata->counts().size());
     for(auto & p: pdata->counts())
         sorted.emplace_back(std::piecewise_construct,
-                            std::forward_as_tuple(p.first + 1), 
+                            std::forward_as_tuple(p.first + 1),
                             std::forward_as_tuple(p.second));
 
     std::sort(sorted.begin(), sorted.end(),
