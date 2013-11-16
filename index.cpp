ac18c8ca9f37a7dc6ae157f21899e36de4b08913
Chase Geigle <geigle1@illinois.edu>
2013-11-15 23:58:20 -0600

    Add simple app that only creates/loads an index.

---
+++ index.cpp
@@ -0,0 +1,30 @@
+/**
+ * @file index.cpp
+ */
+
+#include <iostream>
+
+#include "util/common.h"
+#include "index/inverted_index.h"
+#include "caching/all.h"
+
+using namespace meta;
+
+int main(int argc, char* argv[])
+{
+    if(argc != 2)
+    {
+        std::cerr << "Usage:\t" << argv[0] << " configFile" << std::endl;
+        return 1;
+    }
+
+    auto time = common::time([&]() {
+        auto idx = index::make_index<index::inverted_index,
+                                     caching::splay_cache>(argv[1], uint32_t{10000});
+    });
+
+    std::cout << "Index generation took: " << time.count() / 1000.0
+        << " seconds" << std::endl;
+
+    return 0;
+}
