00860b8445baf461a93afd9cd29078ce7d292cc3
Chase Geigle <geigle1@illinois.edu>
2014-01-22 23:19:54 -0600

    New class: vocabulary_map_writer.
    Allows for building of a B+-tree from a series of sorted insertions (e.g.,
    bulk loading). This will be used in the inverted index to store the
    mapping from strings to term ids, and vice versa.

---
+++ include/index/vocabulary_map_writer.h
@@ -0,0 +1,38 @@
+/**
+ * @file vocabulary_map_writer.h
+ * @author Chase Geigle
+ */
+
+#ifndef _META_LEXICON_H_
+#define _META_LEXICON_H_
+
+#include "index/inverted_index.h"
+
+namespace meta
+{
+namespace index
+{
+
+class vocabulary_map_writer
+{
+  private:
+    std::ofstream file_;
+    std::string path_;
+    uint16_t block_size_;
+    uint64_t num_terms_;
+    uint16_t remaining_block_space_;
+    uint64_t written_nodes_;
+
+  public:
+    vocabulary_map_writer(const std::string& path, uint16_t block_size = 4096);
+    ~vocabulary_map_writer();
+
+    void insert(const std::string& term);
+
+  private:
+    void write_padding();
+    void flush();
+};
+}
+}
+#endif
