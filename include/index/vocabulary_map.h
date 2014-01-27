fdb683361b3b6c89c956f2ffc73f8f96190c2843
Chase Geigle <sky@skystrife.com>
2014-01-26 18:11:34 -0600

    Add doxygen to vocabulary_map* classes.

--- include/index/vocabulary_map.h
+++ include/index/vocabulary_map.h
@@ -11,36 +11,93 @@
 
 namespace meta
 {
-
 namespace util
 {
 template <class>
 class optional;
 }
-
 }
-
 
 namespace meta
 {
 namespace index
 {
 
+/**
+ * A read-only view of a B+-tree-like structure that stores the vocabulary
+ * for an index. It reads the file format that is written by the
+ * vocabulary_map_writer class (see the documentation for the writer for
+ * information about the file format).
+ */
 class vocabulary_map
 {
   private:
+    /**
+     * The file containing the tree. mmapped for performance.
+     */
     io::mmap_file file_;
+
+    /**
+     * Byte positions for each term in the leaves to allow for reverse
+     * lookup of a the string associated with a given id.
+     */
     util::disk_vector<uint64_t> inverse_;
+
+    /**
+     * The size of the nodes in the tree.
+     */
     uint64_t block_size_;
+
+    /**
+     * The ending position of the leaf nodes. Used to determine when to
+     * stop a search.
+     */
     uint64_t leaf_end_pos_;
+
+    /**
+     * The position of the first internal node that is not the root. Used
+     * to seek to the first level during search.
+     */
     uint64_t initial_seek_pos_;
 
-    int compare(const std::string & term, const char * other) const;
+    /**
+     * Convenience wrapper for comparing the term with strings in the tree.
+     * @param term  the term we are looking for
+     * @param other the string in the tree we are considering
+     */
+    int compare(const std::string& term, const char* other) const;
 
   public:
-    vocabulary_map(const std::string & path, uint16_t block_size = 4096);
-    util::optional<term_id> find(const std::string & term) const;
+    /**
+     * Creates a vocabulary map reading the file in the given path with
+     * the given block size. Changing the block size is not
+     * recommended---the block size used should always be the same as the
+     * block size used in the vocabulary_map_writer used to create the
+     * tree.
+     *
+     * @param path the location of the tree file
+     * @param block_size the size of the nodes in the tree
+     */
+    vocabulary_map(const std::string& path, uint16_t block_size = 4096);
+
+    /**
+     * Finds the given term in the tree, if it exists.
+     * @param term the term to find an id for
+     */
+    util::optional<term_id> find(const std::string& term) const;
+
+    /**
+     * Finds the term associated with the given id. No bounds checking is
+     * performed---accessing beyond the maximum assigned term_id is
+     * undefined behavior.
+     *
+     * @param t_id the term id to find the string representation of
+     */
     std::string find_term(term_id t_id) const;
+
+    /**
+     * The number of terms in the map.
+     */
     uint64_t size() const;
 };
 }
