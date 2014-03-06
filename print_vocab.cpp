8e8510361486730c90ca0e5e8c916e3121d8a80f
Chase Geigle <geigle1@illinois.edu>
2014-03-06 17:34:20 -0600

    Add sample applications for printing and searching vocab maps.

---
+++ print_vocab.cpp
@@ -0,0 +1,17 @@
+#include "index/vocabulary_map.h"
+
+using namespace meta;
+
+int main(int argc, char** argv)
+{
+    if (argc < 2)
+    {
+        std::cout << "Usage: " << argv[0] << " filename" << std::endl;
+        return 1;
+    }
+
+    index::vocabulary_map map{argv[1]};
+
+    for (term_id tid{0}; tid < map.size(); ++tid)
+        std::cout << map.find_term(tid) << std::endl;
+}
