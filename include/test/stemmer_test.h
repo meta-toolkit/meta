aba8156e1d3c40d263f6d3e7e21b46f4bed8ef24
Sean Massung <massung1@illinois.edu>
2014-03-10 10:42:00 -0500

    remove meta::stemmers (deprecated by filters)

--- include/test/stemmer_test.h
+++ include/test/stemmer_test.h
@@ -12,17 +12,11 @@
 #include <fstream>
 
 #include "test/unit_test.h"
-#include "stemmers/porter2.h"
-#include "stemmers/no_stemmer.h"
 
 namespace meta
 {
 namespace testing
 {
-
-template <class Stemmer>
-void test_stem(Stemmer& stemmer, std::ifstream& in, bool do_stem);
-
 int stemmer_tests();
 }
 }
--- src/test/stemmer_test.cpp
+++ src/test/stemmer_test.cpp
@@ -3,6 +3,7 @@
  * @author Sean Massung
  */
 
+#include "porter2_stemmer.h"
 #include "test/stemmer_test.h"
 
 namespace meta
@@ -10,48 +11,30 @@ namespace meta
 namespace testing
 {
 
-template <class Stemmer>
-void test_stem(Stemmer& stemmer, std::ifstream& in, bool do_stem)
-{
-    std::string to_stem;
-    std::string stemmed;
-    while (in >> to_stem >> stemmed)
-    {
-        std::string orig{to_stem};
-        stemmer(to_stem);
-        if (do_stem)
-            ASSERT_EQUAL(to_stem, stemmed);
-        else
-            ASSERT_EQUAL(to_stem, orig);
-    }
-}
-
 int stemmer_tests()
 {
     int num_failed = 0;
 
-    num_failed += testing::run_test("no-stemmer", [&]()
-    {
-        std::ifstream in{"../data/porter2_stems.txt"};
-        stemmers::no_stemmer stemmer;
-        test_stem(stemmer, in, false);
-    });
-
     num_failed += testing::run_test("porter2-stemmer", [&]()
     {
         std::ifstream in{"../data/porter2_stems.txt"};
-        stemmers::porter2 stemmer;
-        test_stem(stemmer, in, true);
+        std::string to_stem;
+        std::string stemmed;
+        while (in >> to_stem >> stemmed)
+        {
+            std::string orig{to_stem};
+            Porter2Stemmer::stem(to_stem);
+            ASSERT_EQUAL(to_stem, stemmed);
+        }
     });
 
     num_failed += testing::run_test("porter2-special-cases", [&]()
     {
-        stemmers::porter2 stemmer;
         const static auto unchanged = {"'", "q", "<s>", "</s>"};
         for (auto& w : unchanged)
         {
             std::string to_stem{w};
-            stemmer(to_stem);
+            Porter2Stemmer::stem(to_stem);
             ASSERT_EQUAL(to_stem, w);
         }
     });
