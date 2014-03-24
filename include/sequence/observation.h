1ba36781b021ec90d2112045b71337bfa94625e1
Chase Geigle <geigle1@illinois.edu>
2014-03-24 15:01:55 -0500

    Begin work on sequence labeling component.

---
+++ include/sequence/observation.h
@@ -0,0 +1,85 @@
+/**
+ * @file sequence.h
+ * @author Chase Geigle
+ *
+ * All files in META are released under the MIT license. For more details,
+ * consult the file LICENSE in the root of the project.
+ */
+
+#ifndef META_OBSERVATION_H_
+#define META_OBSERVATION_H_
+
+#include <string>
+
+#include "util/identifiers.h"
+#include "util/optional.h"
+
+namespace meta
+{
+namespace sequence
+{
+
+MAKE_IDENTIFIER(symbol_t, std::string)
+MAKE_IDENTIFIER(tag_t, std::string)
+
+/**
+ * Represents an observation in a tagged sequence. Contains a symbol and
+ * (optionally) a tag for that symbol.
+ */
+class observation
+{
+  public:
+    /**
+     * Constructs an observation with a tag.
+     * @param sym The symbol for the observation
+     * @param t The tag for the observation
+     */
+    observation(symbol_t sym, tag_t t);
+
+    /**
+     * Constructs an observation that does not yet have a tag.
+     * @param sym The symbol for the observation
+     */
+    observation(symbol_t sym);
+
+    /**
+     * @return the symbol for this observation
+     */
+    const symbol_t& symbol() const;
+
+    /**
+     * @throw exception if there is no tag
+     * @return the tag for this observation
+     */
+    const tag_t& tag() const;
+
+    /**
+     * Sets the current symbol
+     * @param sym The new symbol for this observation
+     */
+    void symbol(symbol_t sym);
+
+    /**
+     * Sets the current tag
+     * @param t The new tag for this observation
+     */
+    void tag(tag_t t);
+
+    /**
+     * Basic exception class for observation interactions.
+     */
+    class exception : public std::runtime_error
+    {
+      public:
+        using std::runtime_error::runtime_error;
+    };
+
+  private:
+    /// The symbol for this observation
+    symbol_t symbol_;
+    /// The tag for this observation, if it exists
+    util::optional<tag_t> tag_;
+};
+}
+}
+#endif
---
+++ src/sequence/observation.cpp
@@ -0,0 +1,47 @@
+/**
+ * @file sequence.cpp
+ * @author Chase Geigle
+ */
+
+#include "sequence/observation.h"
+
+namespace meta
+{
+namespace sequence
+{
+
+observation::observation(symbol_t sym, tag_t t)
+    : symbol_{std::move(sym)}, tag_{std::move(t)}
+{
+    // nothing
+}
+
+observation::observation(symbol_t sym)
+    : symbol_{std::move(sym)}, tag_{util::nullopt}
+{
+    // nothing
+}
+
+const symbol_t& observation::symbol() const
+{
+    return symbol_;
+}
+
+const tag_t& observation::tag() const
+{
+    if (tag_)
+        return *tag_;
+    throw exception{"no tag for this observation"};
+}
+
+void observation::symbol(symbol_t sym)
+{
+    symbol_ = std::move(sym);
+}
+
+void observation::tag(tag_t t)
+{
+    tag_ = std::move(t);
+}
+}
+}
