1924cc3e4cd87531bc48d9b53184a2cdc9efaf73
Chase Geigle <geigle1@illinois.edu>
2014-04-22 13:33:02 -0500

    Update crf-train to use the configuration file.

--- crf-train.cpp
+++ crf-train.cpp
@@ -7,6 +7,7 @@
 #include "sequence/crf.h"
 #include "sequence/io/ptb_parser.h"
 #include "util/filesystem.h"
+#include "cpptoml.h"
 
 using namespace meta;
 
@@ -21,25 +22,82 @@ int main(int argc, char** argv)
 {
     if (argc < 2)
     {
-        std::cout << "Usage: " << argv[0] << " path-to-treebank" << std::endl;
+        std::cerr << "Usage: " << argv[0] << " config.toml" << std::endl;
         return 1;
     }
 
     logging::set_cerr_logging();
 
-    std::string path = argv[1];
-    path += "/treebank-2/tagged/wsj";
+    auto config = cpptoml::parse_file(argv[1]);
+
+    auto prefix = config.get_as<std::string>("prefix");
+    if (!prefix)
+    {
+        LOG(fatal) << "Global configuration must have a prefix key" << ENDLG;
+        return 1;
+    }
+
+    auto crf_grp = config.get_group("crf");
+    if (!crf_grp)
+    {
+        LOG(fatal) << "Configuration must contain a [crf] group" << ENDLG;
+        return 1;
+    }
+
+    auto crf_prefix = crf_grp->get_as<std::string>("prefix");
+    if (!crf_prefix)
+    {
+        LOG(fatal) << "[crf] group must contain a prefix to store model files"
+                   << ENDLG;
+        return 1;
+    }
+
+    auto treebank = crf_grp->get_as<std::string>("treebank");
+    if (!treebank)
+    {
+        LOG(fatal) << "[crf] group must contain a treebank path" << ENDLG;
+        return 1;
+    }
+
+    auto corpus = crf_grp->get_as<std::string>("corpus");
+    if (!corpus)
+    {
+        LOG(fatal) << "[crf] group must contain a corpus" << ENDLG;
+        return 1;
+    }
+
+    auto train_sections = crf_grp->get_array("train-sections");
+    if (!train_sections)
+    {
+        LOG(fatal) << "[crf] group must contain train-sections" << ENDLG;
+        return 1;
+    }
+
+    auto section_size = crf_grp->get_as<int64_t>("section-size");
+    if (!section_size)
+    {
+        LOG(fatal) << "[crf] group must contain section-size" << ENDLG;
+        return 1;
+    }
+
+    std::string path =
+        *prefix + "/" + *treebank + "/treebank-2/tagged/" + *corpus;
 
     std::vector<sequence::sequence> training;
     {
-        printing::progress progress{" > Reading training data: ", 18 * 99};
-        for (uint8_t i = 0; i <= 18; ++i)
+        auto begin = train_sections->at(0)->as<int64_t>()->value();
+        auto end = train_sections->at(1)->as<int64_t>()->value();
+        LOG(info) << "beg: " << begin << ", end: " << end
+                  << ", sec: " << *section_size << ENDLG;
+        printing::progress progress(" > Reading training data: ",
+                                    (end - begin + 1) * *section_size);
+        for (uint8_t i = begin; i <= end; ++i)
         {
             auto folder = two_digit(i);
-            for (uint8_t j = 0; j <= 99; ++j)
+            for (uint8_t j = 0; j <= *section_size; ++j)
             {
                 progress(i * 99 + j);
-                auto file = "wsj_" + folder + two_digit(j) + ".pos";
+                auto file = *corpus + "_" + folder + two_digit(j) + ".pos";
                 auto filename = path + "/" + folder + "/" + file;
                 auto sequences = sequence::extract_sequences(filename);
                 for (auto & seq : sequences)
@@ -50,7 +108,6 @@ int main(int argc, char** argv)
 
     filesystem::make_directory("crf");
     auto analyzer = sequence::default_pos_analyzer("crf");
-
     {
         printing::progress progress{" > Generating features: ",
                                     training.size()};
@@ -63,7 +120,7 @@ int main(int argc, char** argv)
     }
     analyzer.save();
 
-    sequence::crf crf{"crf"};
+    sequence::crf crf{*crf_prefix};
     crf.train({}, training);
 
     return 0;
