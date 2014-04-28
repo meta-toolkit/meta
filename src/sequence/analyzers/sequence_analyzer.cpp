4aa59591f71c1d2af98da409c349801b064d0c76
Chase Geigle <geigle1@illinois.edu>
2014-04-28 11:55:22 -0500

    Add more features to default POS tagging sequence analyzer.
    Achieves 97% accuracy on the testing section of the WSJ when trained with
    the current CRF implementation (in ~290 epochs).

--- src/sequence/analyzers/sequence_analyzer.cpp
+++ src/sequence/analyzers/sequence_analyzer.cpp
@@ -8,6 +8,7 @@
 #include <sstream>
 #include "io/binary.h"
 #include "sequence/analyzers/sequence_analyzer.h"
+#include "utf/utf.h"
 #include "util/filesystem.h"
 #include "util/mapping.h"
 #include "util/progress.h"
@@ -103,15 +104,10 @@ void sequence_analyzer::analyze(sequence& sequence) const
             fn(sequence, t, coll);
 
         if (!label_id_mapping_.contains_key(sequence[t].tag()))
-        {
             sequence[t].label(label_id(label_id_mapping_.size()));
-        }
         else
-        {
             sequence[t].label(label_id_mapping_.get_value(sequence[t].tag()));
-        }
     }
-
 }
 
 feature_id sequence_analyzer::feature(const std::string& feature)
@@ -170,21 +166,64 @@ std::string suffix(const std::string& input, uint64_t length)
         return input;
     return input.substr(input.size() - length);
 }
+
+std::string prefix(const std::string& input, uint64_t length)
+{
+    if (length > input.size())
+        return {input.begin(), input.end()};
+    return {input.begin(), input.begin() + length};
+}
 }
 
-sequence_analyzer default_pos_analyzer(const std::string& prefix)
+sequence_analyzer default_pos_analyzer(const std::string& folder)
 {
-    // TODO: add more features, look at the MeLT paper for some
-    sequence_analyzer analyzer{prefix};
+    sequence_analyzer analyzer{folder};
+
+    auto word_feats = [](const std::string& word, uint64_t t,
+                         sequence_analyzer::collector& coll)
+    {
+        auto norm = utf::foldcase(word);
+        for (int i = 1; i <= 4; i++)
+        {
+            auto len = std::to_string(i);
+            coll.add("w[t]_suffix_" + len + "=" + suffix(norm, i), 1);
+            coll.add("w[t]_prefix_" + len + "=" + prefix(norm, i), 1);
+        }
+        coll.add("w[t]=" + norm, 1);
+
+        // additional binary word features
+        if (std::any_of(word.begin(), word.end(), [](char c)
+        { return std::isdigit(c); }))
+        {
+            coll.add("w[t]_has_digit=1", 1);
+        }
+
+        if (std::find(word.begin(), word.end(), '-') != word.end())
+            coll.add("w[t]_has_hyphen=1", 1);
+
+        if (std::any_of(word.begin(), word.end(), [](char c)
+        { return std::isupper(c); }))
+        {
+            coll.add("w[t]_has_upper=1", 1);
+            if (t != 0)
+            {
+                coll.add("w[t]_has_upper_and_not_sentence_start=1", 1);
+            }
+        }
+
+        if (std::all_of(word.begin(), word.end(), [](char c)
+        { return std::isupper(c); }))
+        {
+            coll.add("w[t]_all_upper=1", 1);
+        }
+    };
 
     // current word features
-    analyzer.add_observation_function([](const sequence& seq, uint64_t t,
-                                         sequence_analyzer::collector& coll)
+    analyzer.add_observation_function([=](const sequence& seq, uint64_t t,
+                                          sequence_analyzer::collector& coll)
     {
         std::string word = seq[t].symbol();
-        coll.add("suffix=" + suffix(word, 3), 1);
-        coll.add(std::string{"prefix1="} + word[0], 1);
-        coll.add("w[t]=" + word, 1);
+        word_feats(word, t, coll);
     });
 
     // previous word features
@@ -192,42 +231,58 @@ sequence_analyzer default_pos_analyzer(const std::string& prefix)
                                          sequence_analyzer::collector& coll)
     {
         std::string word = seq[t].symbol();
-        auto prevword = std::string{"-START-"};
-        auto prev2word = std::string{"-START2-"};
         if (t > 0)
         {
-            prevword = seq[t-1].symbol();
-            prev2word = "-START-";
+            auto prevword = seq[t - 1].symbol();
+            coll.add("w[t-1]=" + utf::foldcase(prevword), 1);
+            if (t > 1)
+            {
+                auto prev2word = seq[t - 2].symbol();
+                coll.add("w[t-2]=" + utf::foldcase(prev2word), 1);
+            }
+            else
+            {
+                coll.add("w[t-2]=<s>", 1);
+            }
         }
-        if (t > 1)
-            prev2word = seq[t-2].symbol();
-
-        coll.add("w[t-1]=" + prevword, 1);
-        coll.add("w[t-1]suffix=" + suffix(prevword, 3), 1);
-        coll.add("w[t-2]=" + prev2word, 1);
+        else
+        {
+            coll.add("w[t-1]=<s>", 1);
+            coll.add("w[t-2]=<s1>", 1);
+        }
     });
 
     // next word features
     analyzer.add_observation_function([](const sequence& seq, uint64_t t,
                                          sequence_analyzer::collector& coll)
     {
-        auto nextword = std::string{"-END-"};
-        auto next2word = std::string{"-END2-"};
         if (t + 1 < seq.size())
         {
-            nextword = seq[t+1].symbol();
-            next2word = "-END-";
+            auto nextword = seq[t + 1].symbol();
+            coll.add("w[t+1]=" + utf::foldcase(nextword), 1);
+            if (t + 2 < seq.size())
+            {
+                auto next2word = seq[t + 2].symbol();
+                coll.add("w[t+2]=" + utf::foldcase(next2word), 1);
+            }
+            else
+            {
+                coll.add("w[t+2]=</s>", 1);
+            }
         }
-        if (t + 2 < seq.size())
-            next2word = seq[t+2].symbol();
-
-        coll.add("w[t+1]=" + nextword, 1);
-        coll.add("w[t+1]suffix=" + suffix(nextword, 3), 1);
-        coll.add("w[t+2]=" + next2word, 1);
+        else
+        {
+            coll.add("w[t+1]=</s>", 1);
+            coll.add("w[t+2]=</s1>", 1);
+        }
     });
+
+    // bias term
+    analyzer.add_observation_function([](const sequence&, uint64_t,
+                                         sequence_analyzer::collector& coll)
+    { coll.add("bias", 1); });
 
     return analyzer;
 }
-
 }
 }
