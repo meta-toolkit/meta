631db209e93d3229436ea55e98cc5a940a20118f
Sean Massung <massung1@illinois.edu>
2014-09-09 17:00:10 -0500

    work on lm-test exe

--- src/lm/tools/lm-test.cpp
+++ src/lm/tools/lm-test.cpp
@@ -4,6 +4,7 @@
  */
 
 #include <iostream>
+#include <algorithm>
 #include "meta.h"
 #include "lm/language_model.h"
 
@@ -13,7 +14,7 @@ template <class C>
 std::string make_string(const C& cont)
 {
     std::string ret = "";
-    for(auto& e: cont)
+    for (auto& e : cont)
         ret += e + " ";
     return ret;
 }
@@ -29,7 +30,30 @@ int main(int argc, char* argv[])
         std::cout << "> ";
         std::getline(std::cin, line);
         auto lma = model.analysis(line);
-        for(auto& p: lma)
+        for (auto& p : lma)
             std::cout << make_string(p.first) << ": " << p.second << std::endl;
+
+        std::cout << std::endl;
+
+        using pair_t = decltype(lma[0]);
+        auto it = std::min_element(lma.begin(), lma.end(),
+            [&](const pair_t& a, const pair_t& b) {
+                return a.second < b.second;
+            }
+        );
+
+        auto bad_one = it->first;
+        std::cout << "You might want to modify/remove the word \""
+           << bad_one.back() << "\"" << std::endl;
+
+        bad_one.pop_back();
+        std::cout << "Candidates:" << std::endl;
+        size_t i = 1;
+        for(auto& p: model.top_k(bad_one, 5))
+        {
+
+            std::cout << " " << i << ". " << p.first << std::endl;
+            ++i;
+        }
     }
 }
