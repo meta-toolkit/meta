770e843d8d6785ff153d293f01f84c7490c8a0ac
Sean Massung <massung1@illinois.edu>
2014-11-19 12:19:30 -0600

    (issue #46) ngram_pos_analyzer uses MeTA's CRF

--- include/analyzers/ngram/ngram_pos_analyzer.h
+++ include/analyzers/ngram/ngram_pos_analyzer.h
@@ -9,8 +9,11 @@
 #ifndef META_NGRAM_POS_ANALYZER_H_
 #define META_NGRAM_POS_ANALYZER_H_
 
+#include <string>
 #include "analyzers/analyzer_factory.h"
-#include "analyzers/ngram/ngram_simple_analyzer.h"
+#include "sequence/analyzers/sequence_analyzer.h"
+#include "analyzers/ngram/ngram_analyzer.h"
+#include "sequence/crf/crf.h"
 #include "util/clonable.h"
 
 namespace meta
@@ -20,20 +23,33 @@ namespace analyzers
 
 /**
  * Analyzes documents based on part-of-speech tags instead of words.
+ * The recommended tokenizer for use with this analyzer is icu-tokenizer with no
+ * other filters added. This tokenizer should be used to ensure that capital
+ * letters and such may be used as features. Function words and stop words
+ * should *not* be removed and words should not be stemmed for the same reason.
  */
 class ngram_pos_analyzer : public util::multilevel_clonable
-                           <analyzer, ngram_simple_analyzer, ngram_pos_analyzer>
+                           <analyzer, ngram_analyzer, ngram_pos_analyzer>
 {
     using base = util::multilevel_clonable<analyzer,
-                                           ngram_simple_analyzer,
+                                           ngram_analyzer,
                                            ngram_pos_analyzer>;
 
   public:
     /**
      * Constructor.
-     * @param n The value of n in ngram.
+     * @param n The value of n to use for the ngrams.
+     * @param stream The stream to read tokens from.
+     * @param crf_prefix
      */
-    ngram_pos_analyzer(uint16_t n);
+    ngram_pos_analyzer(uint16_t n, std::unique_ptr<token_stream> stream,
+            const std::string& crf_prefix);
+
+    /**
+     * Copy constructor.
+     * @param other The other ngram_pos_analyzer to copy from
+     */
+    ngram_pos_analyzer(const ngram_pos_analyzer& other);
 
     /**
      * Tokenizes a file into a document.
@@ -43,6 +59,16 @@ class ngram_pos_analyzer : public util::multilevel_clonable
 
     /// Identifier for this analyzer.
     const static std::string id;
+
+  private:
+    /// The token stream to be used for extracting tokens
+    std::unique_ptr<token_stream> stream_;
+
+    /// The CRF used to tag the sentences
+    std::shared_ptr<sequence::crf> crf_;
+
+    /// Generates features for the CRF; const indicates testing mode
+    const sequence::sequence_analyzer seq_analyzer_;
 };
 
 /**
