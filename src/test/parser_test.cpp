822f4fbbc32485898d687a5a87a799a216370f11
Chase Geigle <sky@skystrife.com>
2014-11-20 19:08:29 -0600

    Add basic unit tests for empty and unary chain removers.

--- src/test/parser_test.cpp
+++ src/test/parser_test.cpp
@@ -16,26 +16,13 @@ namespace meta
 namespace testing
 {
 
-void transform(parser::parse_tree&)
-{
-    return;
-}
-
-template <class Head, class... Rest>
-void transform(parser::parse_tree& tree, Head&& head, Rest&&... rest)
-{
-    tree.transform(head);
-    transform(tree, rest...);
-}
-
-template <class... Transformers>
 void assert_tree_equal(std::string input, std::string expected,
-                        Transformers&&... trans)
+                       parser::tree_transformer& trns)
 {
     std::stringstream in_ss{input};
     auto in_trees = parser::io::extract_trees(in_ss);
 
-    transform(in_trees.front(), trans...);
+    in_trees.front().transform(trns);
 
     std::stringstream exp_ss{expected};
     auto exp_trees = parser::io::extract_trees(exp_ss);
@@ -50,13 +37,31 @@ int transformer_tests()
     auto num_failed = int{0};
 
     annotation_remover ann_remover;
+    empty_remover empty_rem;
+    unary_chain_remover uchain_rem;
 
-    num_failed += testing::run_test("annotation_remover_basic", [&]()
+    num_failed += testing::run_test("annotation_remover", [&]()
                                     {
         auto tree = "((X (Y (Z-XXX (Y z))) (Z|Q (Y=1 (X x)))))";
-        auto tree_noann = "((X (Y (Z     (Y z))) (Z   (Y   (X x)))))";
+        auto tree_noann = "((X (Y (Z (Y z))) (Z (Y (X x)))))";
 
         assert_tree_equal(tree, tree_noann, ann_remover);
+    });
+
+    num_failed += testing::run_test("empty_remover", [&]()
+                                    {
+        auto tree = "((X (Y (-NONE- *)) (Z z) (W (Y (-NONE- *) (Q q)))))";
+        auto tree_noempty = "((X (Z z) (W (Y (Q q)))))";
+
+        assert_tree_equal(tree, tree_noempty, empty_rem);
+    });
+
+    num_failed += testing::run_test("unary_chain_remover", [&]()
+                                    {
+        auto tree = "((X (X (X (Y y) (Z z)) (X (X (X x))))))";
+        auto tree_nochain = "((X (X (Y y) (Z z)) (X x)))";
+
+        assert_tree_equal(tree, tree_nochain, uchain_rem);
     });
 
     return num_failed;
