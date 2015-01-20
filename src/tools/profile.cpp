a8d9482db78613947017573935859c103d8e746c
Sean Massung <massung1@illinois.edu>
2015-01-20 14:59:42 -0600

    create profile skeleton

---
+++ src/tools/profile.cpp
@@ -0,0 +1,13 @@
+/**
+ * @file profile.cpp
+ * @author Sean Massung
+ */
+
+#include <iostream>
+#include "logging/logger.h"
+#include "meta.h"
+
+int main(int argc, char* argv[])
+{
+    std::cout << "Yo" << std::endl;
+}
