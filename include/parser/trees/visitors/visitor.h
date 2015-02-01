107f0b37fba072bc916134d98c9346310748a02e
Chase Geigle <sky@skystrife.com>
2015-02-01 00:28:26 -0600

    Fully comment visitor and const_visitor.

--- include/parser/trees/visitors/visitor.h
+++ include/parser/trees/visitors/visitor.h
@@ -17,21 +17,53 @@ namespace parser
 class leaf_node;
 class internal_node;
 
+/**
+ * Abstract base class for visitors over parse trees that do not modify
+ * the underlying tree.
+ */
 template <class T>
 class const_visitor
 {
   public:
+    /**
+     * The result of running the visitor over the tree.
+     */
     using result_type = T;
+
+    /**
+     * @return the result of running the visitor on the supplied leaf node
+     */
     virtual result_type operator()(const leaf_node&) = 0;
+
+    /**
+     * @return the result of running the visitor on the supplied internal
+     * node
+     */
     virtual result_type operator()(const internal_node&) = 0;
 };
 
+/**
+ * Abstract base class for visitors over parse trees that are allowed to
+ * modify the underlying tree.
+ */
 template <class T>
 class visitor
 {
   public:
+    /**
+     * The result of running the visitor over the tree.
+     */
     using result_type = T;
+
+    /**
+     * @return the result of running the visitor on the supplied leaf node
+     */
     virtual result_type operator()(leaf_node&) = 0;
+
+    /**
+     * @return the result of running the visitor on the supplied internal
+     * node
+     */
     virtual result_type operator()(internal_node&) = 0;
 };
 }
