e3cb6ef49ea1d533cc479357c8e75eb2cd83c19e
Chase Geigle <sky@skystrife.com>
2015-02-01 00:24:04 -0600

    Fully comment multi_transformer.

--- include/parser/trees/visitors/multi_transformer.h
+++ include/parser/trees/visitors/multi_transformer.h
@@ -21,6 +21,10 @@ namespace meta
 namespace parser
 {
 
+/**
+ * A template class for composing tree transformers. Each template
+ * parameter is another transform to be run.
+ */
 template <class... Transformers>
 class multi_transformer : public tree_transformer
 {
@@ -29,11 +33,19 @@ class multi_transformer : public tree_transformer
                                                "be specified with at least two "
                                                "transformers to be run");
 
+    /**
+     * Constructs each of the composed transformers using their default
+     * constructor.
+     */
     multi_transformer() : transforms_{{make_unique<Transformers>()...}}
     {
         // nothing
     }
 
+    /**
+     * Take a set of pointers to the transformers to be composed.
+     * @param trans The transformers to be used
+     */
     multi_transformer(std::unique_ptr<Transformers>... trans)
         : transforms_{trans...}
     {
@@ -51,6 +63,11 @@ class multi_transformer : public tree_transformer
     }
 
   private:
+    /**
+     * Runs each of the transformers on a node in turn.
+     * @param n The node to transform
+     * @return the transformed node
+     */
     std::unique_ptr<node> run_transforms(const node& n)
     {
         auto res = n.accept(*transforms_[0]);
@@ -59,10 +76,20 @@ class multi_transformer : public tree_transformer
         return res;
     }
 
+    /**
+     * The transforms to run.
+     */
     std::array<std::unique_ptr<tree_transformer>, sizeof...(Transformers)>
         transforms_;
 };
 
+/**
+ * Helper function for constructing a multi_transformer from a set of
+ * pointers to transformers.
+ *
+ * @param trans The transformers to be used
+ * @return a multi_transformer with the supplied transforms
+ */
 template <class... Transformers>
 multi_transformer<Transformers...>
     make_transformer(std::unique_ptr<Transformers>... trans)
