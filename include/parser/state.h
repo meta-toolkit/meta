bdffaa5bbffaa920189eb3d0c413b293499fc973
Chase Geigle <sky@skystrife.com>
2015-01-31 23:28:37 -0600

    Fully comment the state class.

--- include/parser/state.h
+++ include/parser/state.h
@@ -23,14 +23,40 @@ namespace meta
 namespace parser
 {
 
+/**
+ * Represents the current parser state of a shift-reduce parser. Parser
+ * states consist of a stack of partial parse trees, a queue of
+ * pre-terminals, and whether or not the state is "finished".
+ *
+ * The stack is represented using a persistent stack structure to ensure
+ * that updates to the parser state occur in O(1) time. The queue is
+ * static, so it can be represented as a vector + an index and updated in
+ * O(1) time.
+ */
 class state
 {
   public:
+    /**
+     * A persistent stack of partial parse trees.
+     */
     using stack_type = util::persistent_stack<std::unique_ptr<node>>;
 
+    /**
+     * Constructs a state from a parse tree. This is used to generate the
+     * starting state for parsing during training.
+     */
     state(const parse_tree& tree);
+
+    /**
+     * Constructs a state from a POS-tagged sequence. This generates teh
+     * starting state for parsing during test time.
+     */
     state(const sequence::sequence& sentence);
 
+    /**
+     * Advances the current state by taking the given transition.
+     * @param trans The transition to take to move the state forward
+     */
     void advance(const transition& trans);
 
     /**
@@ -49,18 +75,52 @@ class state
      */
     transition emergency_transition() const;
 
+    /**
+     * @param depth The depth to seek to in the stack
+     * @return the node on the stack at the given depth
+     */
     const node* stack_item(size_t depth) const;
+
+    /**
+     * @param depth The depth to seek to in the queue
+     * @return the node on teh queue at the given depth
+     */
     const leaf_node* queue_item(ssize_t depth) const;
 
+    /**
+     * @return the number of partial parse trees on the stack.
+     */
     size_t stack_size() const;
+
+    /**
+     * @return the number of preterminals on the queue.
+     */
     size_t queue_size() const;
 
+    /**
+     * @return whether or not this state has finished parsing.
+     */
     bool finalized() const;
 
   private:
+    /**
+     * The stack of partial parse trees.
+     */
     stack_type stack_;
+
+    /**
+     * The queue of preterminals.
+     */
     std::vector<std::unique_ptr<leaf_node>> queue_;
+
+    /**
+     * The index of the front of the queue.
+     */
     size_t q_idx_;
+
+    /**
+     * Whether or not this state has finished parsing.
+     */
     bool done_;
 };
 }
