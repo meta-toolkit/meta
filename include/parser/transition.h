354672af8f3132a2b39bd448ce8027fabdca1a1f
Chase Geigle <sky@skystrife.com>
2015-02-01 00:05:24 -0600

    Actually add comments for transition class.

--- include/parser/transition.h
+++ include/parser/transition.h
@@ -20,9 +20,16 @@ namespace parser
 
 MAKE_NUMERIC_IDENTIFIER(trans_id, uint16_t)
 
+/**
+ * Represents a transition taken by the parser. Consists of a type and, for
+ * UNARY or REDUCE_L and REDUCE_R actions, a label.
+ */
 class transition : public util::comparable<transition>
 {
   public:
+    /**
+     * The transition types.
+     */
     enum class type_t
     {
         SHIFT = 0,
@@ -33,16 +40,43 @@ class transition : public util::comparable<transition>
         IDLE
     };
 
+    /**
+     * Constructs a transition given a type. This constructor should be
+     * used for SHIFT, FINALIZE, and IDLE transitions.
+     *
+     * @param t The transition type
+     */
     transition(type_t t);
 
+    /**
+     * Constructs a transition given a type and label. This constructor
+     * should be used for REDUCE_L, REDUCE_R, and UNARY transitions.
+     *
+     * @param t The transition type
+     * @param lbl The label for the transition.
+     */
     transition(type_t t, class_label lbl);
 
+    /**
+     * @return the type of this transition
+     */
     type_t type() const;
 
+    /**
+     * @return the label for this transition
+     */
     const class_label& label() const;
 
-    bool operator<(const transition& lhs) const;
+    /**
+     * @param rhs The transition to compare against
+     * @return whether the current transition is lexicographically less
+     * than the other transition
+     */
+    bool operator<(const transition& rhs) const;
 
+    /**
+     * Exception thrown during interactions with transitions.
+     */
     class exception : public std::runtime_error
     {
       public:
@@ -50,12 +84,31 @@ class transition : public util::comparable<transition>
     };
 
   private:
+    /**
+     * The type of this transition.
+     */
     type_t type_;
+
+    /**
+     * The label of this transition, if it exists.
+     */
     util::optional<class_label> label_;
 };
 
+/**
+ * Prints a transition to the stream.
+ * @param os The stream to print to
+ * @param trans The transition to be printed
+ * @return the stream
+ */
 std::ostream& operator<<(std::ostream& os, const transition& trans);
 
+/**
+ * Prints a transition type to the stream.
+ * @param os The stream to print to
+ * @param type The transition type to print
+ * @return the stream
+ */
 std::ostream& operator<<(std::ostream& os, const transition::type_t type);
 
 }
