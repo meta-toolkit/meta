1052d93cc69db42814bf390026cb6461f5cddcfd
Chase Geigle <sky@skystrife.com>
2015-06-02 20:33:09 -0500

    Add regression tests for num_lines (issue #90).

--- src/test/unit_tests.cmake
+++ src/test/unit_tests.cmake
@@ -53,3 +53,7 @@ set_tests_properties(graph PROPERTIES TIMEOUT 10 WORKING_DIRECTORY
 add_test(parser ${UNIT_TEST_EXE} parser)
 set_tests_properties(parser PROPERTIES TIMEOUT 10 WORKING_DIRECTORY
                          ${CMAKE_RUNTIME_OUTPUT_DIRECTORY})
+
+add_test(filesystem ${UNIT_TEST_EXE} filesystem)
+set_tests_properties(filesystem PROPERTIES TIMEOUT 10 WORKING_DIRECTORY
+                         ${CMAKE_RUNTIME_OUTPUT_DIRECTORY})
