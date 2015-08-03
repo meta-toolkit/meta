7e3ba57888cefbaa1c849057f444f31c83a50c18
Sean Massung <massung1@illinois.edu>
2015-08-03 15:50:30 -0500

    test creating and reading from binary LM files

--- src/test/lm_test.cpp
+++ src/test/lm_test.cpp
@@ -17,24 +17,32 @@ int lm_tests()
     int num_failed = 0;
     create_config("line");
 
-    num_failed += testing::run_test(
-        "lm-test", [&]()
-        {
-            lm::language_model model{cpptoml::parse_file("test-config.toml")};
-            lm::sentence s1{
-                "<s> I disagree with this statement for several reasons . </s>",
-                false};
-            lm::sentence s2{
-                "<s> I disagree with this octopus for several reasons . </s>",
-                false};
-            lm::sentence s3{"<s> Hello world ! </s>", false};
-            lm::sentence s4{"<s> xyz xyz xyz </s>", false};
+    auto test = [&]()
+    {
+        lm::language_model model{cpptoml::parse_file("test-config.toml")};
+        lm::sentence s1{
+            "<s> I disagree with this statement for several reasons . </s>",
+            false};
+        lm::sentence s2{
+            "<s> I disagree with this octopus for several reasons . </s>",
+            false};
+        lm::sentence s3{"<s> Hello world ! </s>", false};
+        lm::sentence s4{"<s> xyz xyz xyz </s>", false};
 
-            ASSERT_APPROX_EQUAL(model.log_prob(s1),  -5.0682507);
-            ASSERT_APPROX_EQUAL(model.log_prob(s2), -11.7275571);
-            ASSERT_APPROX_EQUAL(model.log_prob(s3), -11.07649517);
-            ASSERT_APPROX_EQUAL(model.log_prob(s4), -16.41804123);
-        });
+        ASSERT_APPROX_EQUAL(model.log_prob(s1), -5.0682507);
+        ASSERT_APPROX_EQUAL(model.log_prob(s2), -11.7275571);
+        ASSERT_APPROX_EQUAL(model.log_prob(s3), -11.07649517);
+        ASSERT_APPROX_EQUAL(model.log_prob(s4), -16.41804123);
+    };
+
+    // recreate binary LM files each test even if they already exist
+    filesystem::delete_file("test-lm-0.binlm");
+    filesystem::delete_file("test-lm-1.binlm");
+    filesystem::delete_file("test-lm-2.binlm");
+    filesystem::delete_file("test-lm-0.strings");
+
+    num_failed += testing::run_test("lm-test", test);
+    num_failed += testing::run_test("lm-test-read-binary", test);
 
     return num_failed;
 }
