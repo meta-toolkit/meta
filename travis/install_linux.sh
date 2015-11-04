721e5f8f5a62c9b6ccffd2d6244cc54095d19e9b
Chase Geigle <geigle1@illinois.edu>
2015-11-03 23:34:48 -0600

    Add --no-check-certificate flag to wget in travis-ci script.

--- travis/install_linux.sh
+++ travis/install_linux.sh
@@ -5,7 +5,7 @@ mkdir $HOME/bin
 export PATH=$HOME/bin:$PATH
 mkdir $HOME/include
 export CPLUS_INCLUDE_PATH=$HOME/include:$CPLUS_INCLUDE_PATH
-wget http://www.cmake.org/files/v3.2/cmake-3.2.2-Linux-x86_64.sh
+wget --no-check-certificate http://www.cmake.org/files/v3.2/cmake-3.2.2-Linux-x86_64.sh
 sh cmake-3.2.2-Linux-x86_64.sh --prefix=$HOME --exclude-subdir
 if [ "`echo $CXX`" == "g++" ]; then
       export CXX=g++-4.8;
