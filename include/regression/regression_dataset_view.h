f6ad864ad5ce60f443a52b789c1221a22be1ddd1
Chase Geigle <geigle1@illinois.edu>
2015-11-05 11:12:04 -0600

    Add operator- to regression_dataset_view.

--- include/regression/regression_dataset_view.h
+++ include/regression/regression_dataset_view.h
@@ -45,6 +45,30 @@ class regression_dataset_view : public learn::dataset_view
     {
         return dset<regression_dataset>().label(instance);
     }
+
+    friend regression_dataset_view operator-(const regression_dataset_view& lhs,
+                                             const regression_dataset_view& rhs)
+    {
+        auto lhs_indices = lhs.indices();
+        auto rhs_indices = rhs.indices();
+        std::sort(std::begin(lhs_indices), std::end(lhs_indices));
+        std::sort(std::begin(rhs_indices), std::end(rhs_indices));
+
+        std::vector<size_type> diff_indices;
+        std::set_difference(std::begin(lhs_indices), std::end(lhs_indices),
+                            std::begin(rhs_indices), std::end(rhs_indices),
+                            std::back_inserter(diff_indices));
+
+        return {lhs, std::move(diff_indices)};
+    }
+
+  private:
+    regression_dataset_view(const regression_dataset_view& rdv,
+                            std::vector<size_type>&& indices)
+        : dataset_view{rdv, std::move(indices)}
+    {
+        // nothing
+    }
 };
 }
 }
