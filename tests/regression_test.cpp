73848f7a2e285a353e972cfbe1b600ecb1412747
Chase Geigle <geigle1@illinois.edu>
2016-01-25 23:35:37 -0600

    Add regression unit tests.
    These are actually, ironically, regression tests for regression.

---
+++ tests/regression_test.cpp
@@ -0,0 +1,105 @@
+/**
+ * @file regression_test.cpp
+ * @author Chase Geigle
+ */
+
+#include "bandit/bandit.h"
+#include "meta/index/make_index.h"
+#include "meta/regression/regressor_factory.h"
+#include "meta/stats/running_stats.h"
+
+using namespace bandit;
+using namespace meta;
+
+namespace {
+void check_cv(const cpptoml::table& cfg,
+              const regression::regression_dataset& dataset,
+              regression::metrics expected) {
+
+    regression::regression_dataset_view rdv{dataset, std::mt19937{47}};
+    auto results = regression::cross_validate(cfg, rdv, 5);
+
+    stats::running_stats mae;
+    stats::running_stats med_ae;
+    stats::running_stats mse;
+    stats::running_stats r2;
+
+    for (const auto& m : results) {
+        mae.add(m.mean_absolute_error);
+        med_ae.add(m.median_absolute_error);
+        mse.add(m.mean_squared_error);
+        r2.add(m.r2_score);
+    }
+
+    AssertThat(mae.mean(), Is().GreaterThan(0.0).And().LessThan(
+                               expected.mean_absolute_error));
+    AssertThat(med_ae.mean(), Is().GreaterThan(0.0).And().LessThan(
+                                  expected.median_absolute_error));
+    AssertThat(mse.mean(), Is().GreaterThan(0.0).And().LessThan(
+                               expected.mean_squared_error));
+    AssertThat(r2.mean(), Is().GreaterThan(expected.r2_score));
+}
+}
+
+go_bandit([]() {
+    auto config = cpptoml::make_table();
+    config->insert("prefix", "../data");
+
+    auto anas = cpptoml::make_table_array();
+    auto ana = cpptoml::make_table();
+    ana->insert("method", "libsvm");
+    anas->push_back(ana);
+    config->insert("analyzers", anas);
+
+    config->insert("dataset", "housing");
+    config->insert("corpus", "libsvm.toml");
+    config->insert("forward-index", "housing-fwd");
+    config->insert("inverted-index", "housing-inv");
+
+    filesystem::remove_all("housing-fwd");
+    auto f_idx = index::make_index<index::forward_index>(*config);
+
+    regression::regression_dataset dataset{
+        f_idx, [&](doc_id did) {
+            return *f_idx->metadata(did).get<double>("response");
+        }};
+
+    describe("[regression] sgd", [&]() {
+        it("should create an SGD regressor with least-squares loss", [&]() {
+            auto cfg = cpptoml::make_table();
+            cfg->insert("method", "sgd");
+            cfg->insert("loss", "least-squares");
+            check_cv(*cfg, dataset, {3.91, 2.65, 32.85, 0.61});
+        });
+
+        it("should create an SGD regressor with huber loss", [&]() {
+            auto cfg = cpptoml::make_table();
+            cfg->insert("method", "sgd");
+            cfg->insert("loss", "huber");
+            check_cv(*cfg, dataset, {4.08, 2.48, 39.58, 0.54});
+        });
+
+        it("should create an SGD regressor with strong L1-regularization",
+           [&]() {
+               auto cfg = cpptoml::make_table();
+               cfg->insert("method", "sgd");
+               cfg->insert("loss", "least-squares");
+               cfg->insert("l2-regularization", 0.0);
+               cfg->insert("l1-regularization", 1e-4);
+               check_cv(*cfg, dataset, {5.00, 3.17, 53.05, 0.37});
+           });
+
+        it("should create an SGD regressor with both L1 and L2 regularization",
+           [&]() {
+               auto cfg = cpptoml::make_table();
+               cfg->insert("method", "sgd");
+               cfg->insert("loss", "least-squares");
+               cfg->insert("l2-regularization", 1e-5);
+               cfg->insert("l1-regularization", 1e-5);
+               check_cv(*cfg, dataset, {3.90, 2.69, 33.08, 0.60});
+           });
+    });
+
+    f_idx = nullptr;
+    filesystem::remove_all("housing-fwd");
+});
--- tests/unit_tests.cmake
+++ tests/unit_tests.cmake
@@ -1,63 +1,87 @@
 add_test(analyzers ${UNIT_TEST_EXE} --only=[analyzers] --reporter=spec)
 set_tests_properties(analyzers PROPERTIES TIMEOUT 10 WORKING_DIRECTORY
                          ${CMAKE_RUNTIME_OUTPUT_DIRECTORY})
+
 add_test(stemmers ${UNIT_TEST_EXE} --only=[stemmers] --reporter=spec)
 set_tests_properties(stemmers PROPERTIES TIMEOUT 10 WORKING_DIRECTORY
                          ${CMAKE_RUNTIME_OUTPUT_DIRECTORY})
+
 add_test(parallel ${UNIT_TEST_EXE} --only=[parallel] --reporter=spec)
 set_tests_properties(parallel PROPERTIES TIMEOUT 30 WORKING_DIRECTORY
                          ${CMAKE_RUNTIME_OUTPUT_DIRECTORY})
+
 add_test(inverted-index ${UNIT_TEST_EXE} --only=[inverted-index] --reporter=spec)
 set_tests_properties(inverted-index PROPERTIES TIMEOUT 30 WORKING_DIRECTORY
                          ${CMAKE_RUNTIME_OUTPUT_DIRECTORY})
+
 add_test(forward-index ${UNIT_TEST_EXE} --only=[forward-index] --reporter=spec)
 set_tests_properties(forward-index PROPERTIES TIMEOUT 30 WORKING_DIRECTORY
                          ${CMAKE_RUNTIME_OUTPUT_DIRECTORY})
+
 add_test(classifier ${UNIT_TEST_EXE} --only=[classifier] --reporter=spec)
 set_tests_properties(classifier PROPERTIES TIMEOUT 100 WORKING_DIRECTORY
                          ${CMAKE_RUNTIME_OUTPUT_DIRECTORY})
+
+add_test(regression ${UNIT_TEST_EXE} --only=[regression] --reporter=spec)
+set_tests_properties(regression PROPERTIES TIMEOUT 100 WORKING_DIRECTORY
+                         ${CMAKE_RUNTIME_OUTPUT_DIRECTORY})
+
 add_test(string-list ${UNIT_TEST_EXE} --only=[string-list] --reporter=spec)
 set_tests_properties(string-list PROPERTIES TIMEOUT 10 WORKING_DIRECTORY
                          ${CMAKE_RUNTIME_OUTPUT_DIRECTORY})
+
 add_test(vocabulary-map ${UNIT_TEST_EXE} --only=[vocabulary-map] --reporter=spec)
 set_tests_properties(vocabulary-map PROPERTIES TIMEOUT 10 WORKING_DIRECTORY
                          ${CMAKE_RUNTIME_OUTPUT_DIRECTORY})
+
 add_test(libsvm-parser ${UNIT_TEST_EXE} --only=[libsvm-parser] --reporter=spec)
 set_tests_properties(libsvm-parser PROPERTIES TIMEOUT 10 WORKING_DIRECTORY
                          ${CMAKE_RUNTIME_OUTPUT_DIRECTORY})
+
 add_test(rankers ${UNIT_TEST_EXE} --only=[rankers] --reporter=spec)
 set_tests_properties(rankers PROPERTIES TIMEOUT 90 WORKING_DIRECTORY
                          ${CMAKE_RUNTIME_OUTPUT_DIRECTORY})
+
 add_test(ir-eval ${UNIT_TEST_EXE} --only=[ir-eval] --reporter=spec)
 set_tests_properties(ir-eval PROPERTIES TIMEOUT 10 WORKING_DIRECTORY
                          ${CMAKE_RUNTIME_OUTPUT_DIRECTORY})
+
 add_test(binary-io ${UNIT_TEST_EXE} --only=[binary-io] --reporter=spec)
 set_tests_properties(binary-io PROPERTIES TIMEOUT 30 WORKING_DIRECTORY
                          ${CMAKE_RUNTIME_OUTPUT_DIRECTORY})
+
 add_test(graph ${UNIT_TEST_EXE} --only=[graph] --reporter=spec)
 set_tests_properties(graph PROPERTIES TIMEOUT 10 WORKING_DIRECTORY
                          ${CMAKE_RUNTIME_OUTPUT_DIRECTORY})
+
 add_test(parser ${UNIT_TEST_EXE} --only=[parser] --reporter=spec)
 set_tests_properties(parser PROPERTIES TIMEOUT 10 WORKING_DIRECTORY
                          ${CMAKE_RUNTIME_OUTPUT_DIRECTORY})
+
 add_test(language-model ${UNIT_TEST_EXE} --only=[language-model] --reporter=spec)
 set_tests_properties(language-model PROPERTIES TIMEOUT 10 WORKING_DIRECTORY
                          ${CMAKE_RUNTIME_OUTPUT_DIRECTORY})
+
 add_test(filesystem ${UNIT_TEST_EXE} --only=[filesystem] --reporter=spec)
 set_tests_properties(filesystem PROPERTIES TIMEOUT 10 WORKING_DIRECTORY
                          ${CMAKE_RUNTIME_OUTPUT_DIRECTORY})
+
 add_test(feature-selection ${UNIT_TEST_EXE} --only=[feature-selection] --reporter=spec)
 set_tests_properties(feature-selection PROPERTIES TIMEOUT 10 WORKING_DIRECTORY
                          ${CMAKE_RUNTIME_OUTPUT_DIRECTORY})
+
 add_test(tokenizer-filter ${UNIT_TEST_EXE} --only=[tokenizer-filter] --reporter=spec)
 set_tests_properties(tokenizer-filter PROPERTIES TIMEOUT 10 WORKING_DIRECTORY
                          ${CMAKE_RUNTIME_OUTPUT_DIRECTORY})
+
 add_test(metadata ${UNIT_TEST_EXE} --only=[metadata] --reporter=spec)
 set_tests_properties(metadata PROPERTIES TIMEOUT 10 WORKING_DIRECTORY
                          ${CMAKE_RUNTIME_OUTPUT_DIRECTORY})
+
 add_test(topics ${UNIT_TEST_EXE} --only=[topics] --reporter=spec)
 set_tests_properties(topics PROPERTIES TIMEOUT 10 WORKING_DIRECTORY
                          ${CMAKE_RUNTIME_OUTPUT_DIRECTORY})
+
 add_test(hashing ${UNIT_TEST_EXE} --only=[hashing] --reporter=spec)
 set_tests_properties(hashing PROPERTIES TIMEOUT 10 WORKING_DIRECTORY
                          ${CMAKE_RUNTIME_OUTPUT_DIRECTORY})
