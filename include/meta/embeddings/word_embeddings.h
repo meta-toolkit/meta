75ddbb919c0dad428dcaa5baf64f775ff37a9267
Chase Geigle <sky@skystrife.com>
2016-02-10 17:06:08 -0600

    Add initial interface for loading word embeddings.

---
+++ include/meta/embeddings/word_embeddings.h
@@ -0,0 +1,104 @@
+/**
+ * @file word_embeddings.h
+ * @author Chase Geigle
+ *
+ * All files in META are dual-licensed under the MIT and NCSA licenses. For more
+ * details, consult the file LICENSE.mit and LICENSE.ncsa in the root of the
+ * project.
+ */
+
+#ifndef META_EMBEDDINGS_WORD_EMBEDDINGS_H_
+#define META_EMBEDDINGS_WORD_EMBEDDINGS_H_
+
+#include <istream>
+#include <stdexcept>
+
+#include "cpptoml.h"
+#include "meta/hashing/probe_map.h"
+#include "meta/util/aligned_allocator.h"
+#include "meta/util/array_view.h"
+#include "meta/util/string_view.h"
+
+namespace meta
+{
+namespace embeddings
+{
+
+struct embedding
+{
+    std::size_t tid;
+    util::array_view<const double> v;
+};
+
+/**
+ * A read-only model for accessing word embeddings.
+ */
+class word_embeddings
+{
+  public:
+    /**
+     * Loads word embeddings from files.
+     *
+     * @param vocab The stream to read the vocabulary from
+     * @param vectors The stream to read the vectors from
+     */
+    word_embeddings(std::istream& vocab, std::istream& vectors);
+
+    /**
+     * Loads word embeddings from parallel streams. The word embeddings
+     * will be component-wise sums of the word embeddings from the two
+     * streams.
+     *
+     * @param vocab The stream to read the vocabulary from
+     * @param first The first stream
+     * @param second The second stream
+     */
+    word_embeddings(std::istream& vocab, std::istream& first,
+                    std::istream& second);
+
+    /**
+     * @param term The term to look up
+     * @return the embedding vector (as an array_view) for the given term,
+     *  or the vector for the unknown word as appropriate
+     */
+    embedding at(util::string_view term) const;
+
+    /**
+     * @param tid The term id to look up
+     * @return the term (as a string_view) represented by that term id
+     */
+    util::string_view term(std::size_t tid) const;
+
+  private:
+    util::array_view<double> vector(std::size_t tid);
+
+    util::array_view<const double> vector(std::size_t tid) const;
+
+    void load_vocab(std::istream& vocab);
+
+    /// The size of the word embeddings
+    const std::size_t vector_size_;
+
+    /// The embeddings matrix
+    util::aligned_vector<double> embeddings_;
+
+    /// A list of all of the strings in the vocabulary, indexed by id
+    util::aligned_vector<std::string> id_to_term_;
+
+    /// A hash table from a term to its id
+    hashing::probe_map<util::string_view, std::size_t> term_to_id_;
+};
+
+/**
+ * Exception thrown when interacting with the word_embeddings model.
+ */
+class word_embeddings_exception : public std::runtime_error
+{
+  public:
+    using std::runtime_error::runtime_error;
+};
+
+word_embeddings load_embeddings(const cpptoml::table& config);
+}
+}
+#endif
