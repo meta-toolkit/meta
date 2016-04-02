58717adea0fc97a2e36c3f0e1f8fd6f617fe279b
Sean Massung <massung1@illinois.edu>
2016-04-02 12:37:03 -0500

    (#118) add PageRank, personalized PageRank and wikipedia demo

--- include/meta/graph/algorithms/centrality.h
+++ include/meta/graph/algorithms/centrality.h
@@ -34,6 +34,35 @@ template <class Graph>
 centrality_result degree_centrality(const Graph& g);
 
 /**
+ * Find the PageRank centrality of each node in the graph via power iteration.
+ * @see http://ilpubs.stanford.edu:8090/422/1/1999-66.pdf
+ * @param g
+ * @param damp The dampening (smoothing) factor
+ * @param max_iters The maximum number of iterations to run the power iteration
+ * @return a collection of (id, centrality) pairs
+ */
+template <class DirectedGraph>
+centrality_result page_rank_centrality(const DirectedGraph& g,
+                                       double damp = 0.85,
+                                       uint64_t max_iters = 100);
+
+/**
+ * Find the Personalized PageRank centrality of each node in the graph via
+ * simulation with respect to a particular node.
+ * @param g
+ * @param center The node that the PageRank is personalized for (i.e., where the
+ * random jumping takes the surfer)
+ * @param damp The dampening (smoothing) factor, which should usually be lower
+ * than the PageRank dampening factor (0.85)
+ * @param num_passes The number of times to run through the entire network
+ * @return a collection of (id, centrality) pairs
+ */
+template <class DirectedGraph>
+centrality_result personalized_page_rank(const DirectedGraph& g, node_id center,
+                                         double damp = 0.66,
+                                         uint64_t num_passes = 3.0);
+
+/**
  * Find the betweenness centrality of each node in the graph using the algorithm
  * from Ulrik Brandes, 2001. This function is parallelized as it takes some time
  * to compute on large networks.
