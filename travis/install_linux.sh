0d8a557635e5c3830c558e99430b82983401c970
yxkemiya <yxkemiya@users.noreply.github.com>
2016-07-12 17:34:33 +0800

    Update CMake to version 3.6.0.
    The latest libcxx needs CMake 3.4.3 or higher.

--- travis/install_linux.sh
+++ travis/install_linux.sh
@@ -5,8 +5,8 @@ mkdir $HOME/bin
 export PATH=$HOME/bin:$PATH
 mkdir $HOME/include
 export CPLUS_INCLUDE_PATH=$HOME/include:$CPLUS_INCLUDE_PATH
-wget --no-check-certificate http://www.cmake.org/files/v3.2/cmake-3.2.2-Linux-x86_64.sh
-sh cmake-3.2.2-Linux-x86_64.sh --prefix=$HOME --exclude-subdir
+wget --no-check-certificate http://www.cmake.org/files/v3.6/cmake-3.6.0-Linux-x86_64.sh
+sh cmake-3.6.0-Linux-x86_64.sh --prefix=$HOME --exclude-subdir
 
 # we have to manually set CC and CXX since travis 'helpfully' clobbers them
 if [ "$COMPILER" = "gcc" ]; then
