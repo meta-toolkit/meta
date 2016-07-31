meta-toolkit/meta

commit feba3bd6c38d988bc72f2a5d2435f6c61e262e71

Silence conversion warnings when building with Clang.
This also adds -Wconversion to the clang builds. GCC is still to noisy to
bother with.

src/index/ranker/lm_ranker.cpp
@@ -19,7 +19,7 @@ const util::string_view language_model_ranker::id = "language-model";
 float language_model_ranker::score_one(const score_data& sd)
 {
     float ps = smoothed_prob(sd);
-    float pc = static_cast<float>(sd.corpus_term_count) / sd.total_terms;
+    float pc = sd.corpus_term_count / sd.total_terms;
     return sd.query_term_weight
            * fastapprox::fastlog(ps / (doc_constant(sd) * pc));
 }

commit 94529fbd84470e635480e4c7f4fe4f002fb3832c

Fix rounding bug in lm_ranker::score_one.
This was most apparent in the Jelinek-Mercer ranker, but impacted all
language model based ranking functions. This was a regression introduced
in feba3bd.

src/index/ranker/lm_ranker.cpp
@@ -19,7 +19,7 @@ const util::string_view language_model_ranker::id = "language-model";
 float language_model_ranker::score_one(const score_data& sd)
 {
     float ps = smoothed_prob(sd);
-    float pc = sd.corpus_term_count / sd.total_terms;
+    float pc = static_cast<float>(sd.corpus_term_count) / sd.total_terms;
     return sd.query_term_weight
            * fastapprox::fastlog(ps / (doc_constant(sd) * pc));
 }

tests/ranker_test.cpp
@@ -4,8 +4,8 @@
  */
 
 #include "bandit/bandit.h"
-#include "meta/corpus/document.h"
 #include "create_config.h"
+#include "meta/corpus/document.h"
 #include "meta/index/ranker/all.h"
 
 using namespace bandit;
@@ -15,6 +15,7 @@ namespace {
 
 template <class Ranker, class Index>
 void test_rank(Ranker& r, Index& idx, const std::string& encoding) {
+    // exhaustive search for each document
     for (size_t i = 0; i < idx.num_docs(); ++i) {
         auto d_id = idx.docs()[i];
         auto path = idx.doc_path(d_id);
@@ -32,6 +33,20 @@ void test_rank(Ranker& r, Index& idx, const std::string& encoding) {
             AssertThat(ranking[0].score,
                        EqualsWithDelta(ranking[1].score, 0.0001));
         }
+    }
+
+    // sanity checks for simple query
+    corpus::document query;
+    query.content("character");
+
+    auto ranking = r.score(idx, query);
+    // ensure there is diversity in the top 10 documents
+    AssertThat(ranking[0].score, Is().GreaterThan(ranking.back().score));
+
+    // check for sorted-ness of ranking
+    for (uint64_t i = 1; i < ranking.size(); ++i) {
+        AssertThat(ranking[i - 1].score,
+                   Is().GreaterThanOrEqualTo(ranking[i].score));
     }
 }
 }