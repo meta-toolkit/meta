0d637ca3d59f90c4b64bbbf0f6b1535a6cdee265
Chase Geigle <sky@skystrife.com>
2016-08-19 18:16:24 -0500

    Add unit tests for feature selection on a binary_dataset_view.

--- tests/feature_selection_test.cpp
+++ tests/feature_selection_test.cpp
@@ -14,28 +14,21 @@
 using namespace bandit;
 using namespace meta;
 
-namespace
-{
+namespace {
 
-template <class Index>
-void test_method(Index& idx, const std::string& method_id)
-{
+template <class DatasetView>
+void run_test(DatasetView& dset_vw, const std::string& method_id, term_id tid) {
     auto config = cpptoml::make_table();
     auto fcfg = cpptoml::make_table();
     fcfg->insert("method", method_id);
     fcfg->insert("prefix", "test-features");
     config->insert("features", fcfg);
 
-    classify::multiclass_dataset dset{idx};
-    classify::multiclass_dataset_view dset_vw(dset);
-
     auto selector = features::make_selector(*config, dset_vw);
     selector->select(20);
     selector->select(50);
     selector->select_percent(0.05);
     selector->select_percent(0.10);
-
-    auto tid = idx->get_term_id("china"); // this term should be selected
 
     AssertThat(selector->selected(tid), IsTrue());
 
@@ -47,6 +40,28 @@ void test_method(Index& idx, const std::string& method_id)
     AssertThat(
         filesystem::file_exists("test-features." + method_id + ".selected"),
         IsTrue());
+}
+
+template <class Index>
+void test_method(Index& idx, const std::string& method_id) {
+
+    classify::multiclass_dataset dset{idx};
+    classify::multiclass_dataset_view dset_vw(dset);
+
+    auto tid = idx->get_term_id("china"); // this term should be selected
+
+    run_test(dset_vw, method_id, tid);
+}
+
+template <class Index>
+void test_method_binary(Index& idx, const std::string& method_id) {
+    classify::binary_dataset dset{
+        idx, [&](doc_id did) { return idx->label(did) == "chinese"_cl; }};
+    classify::binary_dataset_view dset_vw{dset};
+
+    auto tid = idx->get_term_id("china"); // this term should be selected
+
+    run_test(dset_vw, method_id, tid);
 }
 }
 
@@ -76,16 +91,45 @@ go_bandit([]() {
             test_method(f_idx, "odds-ratio");
             test_method(f_idx, "odds-ratio");
         });
+
+        for (const std::string& method_id :
+             {"chi-square", "info-gain", "corr-coef", "odds-ratio"}) {
+            for (const std::string& suffix : {"1", "2", "3", "selected"}) {
+                filesystem::remove_all("test-features." + method_id + "."
+                                       + suffix);
+            }
+        }
+
+        it("should implement chi square (binary)", [&]() {
+            test_method_binary(f_idx, "chi-square");
+            test_method_binary(f_idx, "chi-square");
+        });
+
+        it("should implement information gain (binary)", [&]() {
+            test_method_binary(f_idx, "info-gain");
+            test_method_binary(f_idx, "info-gain");
+        });
+
+        it("should implement correlation coefficient (binary)", [&]() {
+            test_method_binary(f_idx, "corr-coef");
+            test_method_binary(f_idx, "corr-coef");
+        });
+
+        it("should implement odds ratio (binary)", [&]() {
+            test_method_binary(f_idx, "odds-ratio");
+            test_method_binary(f_idx, "odds-ratio");
+        });
+
+        for (const std::string& method_id :
+             {"chi-square", "info-gain", "corr-coef", "odds-ratio"}) {
+            for (const std::string& suffix : {"1", "2", "selected"}) {
+                filesystem::remove_all("test-features." + method_id + "."
+                                       + suffix);
+            }
+        }
+
     });
 
     f_idx = nullptr;
     filesystem::remove_all("ceeaus");
-    for (const std::string& method_id :
-         {"chi-square", "info-gain", "corr-coef", "odds-ratio"})
-    {
-        for (const std::string& suffix : {"1", "2", "3", "selected"})
-        {
-            filesystem::remove_all("test-features." + method_id + "." + suffix);
-        }
-    }
 });
