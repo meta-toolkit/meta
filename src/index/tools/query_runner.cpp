17cc47527704d711e3a7edf5dbc6b7f3e06965a8
Sean Massung <massung1@illinois.edu>
2016-08-21 11:59:16 -0500

    #141 option to output query results in trec_eval format

--- src/index/tools/query_runner.cpp
+++ src/index/tools/query_runner.cpp
@@ -19,6 +19,46 @@
 using namespace meta;
 
 /**
+ * Prints the current search result in a nice, human-readable format with a
+ * simple snippet if possible. The snippet is made from the beginning of the
+ * "content" metadata field.
+ */
+template <class Index, class SearchResult>
+void print_results(const Index& idx, const SearchResult& result,
+                   uint64_t result_num)
+{
+    std::string path{idx->doc_path(result.d_id)};
+    auto output = printing::make_bold(std::to_string(result_num) + ". " + path)
+                  + " (score = " + std::to_string(result.score) + ", docid = "
+                  + std::to_string(result.d_id) + ")";
+    std::cout << output << std::endl;
+    auto mdata = idx->metadata(result.d_id);
+    if (auto content = mdata.template get<std::string>("content"))
+    {
+        auto len = std::min(std::string::size_type{77}, content->size());
+        std::cout << content->substr(0, len) << "..." << std::endl << std::endl;
+    }
+}
+
+/**
+ * Prints the current search result in TREC format, which can be read by the
+ * trec-eval program. A "name" metadata field is required as the document ID.
+ */
+template <class Index, class SearchResult>
+void print_trec(const Index& idx, const SearchResult& result,
+                uint64_t result_num, uint64_t q_id)
+{
+    auto mdata = idx->metadata(result.d_id);
+    if (auto name = mdata.template get<std::string>("name"))
+    {
+        std::cout << q_id << "\t_\t" << *name << "\t" << result_num << "\t"
+                  << result.score << "\tMeTA" << std::endl;
+    }
+    else
+        throw std::runtime_error{"\"name\" metadata field is required"};
+}
+
+/**
  * Demo app to read a file with one query per line and run each query on an
  * inverted index.
  */
@@ -44,20 +84,36 @@ int main(int argc, char* argv[])
     // Create a ranking class based on the config file.
     auto group = config->get_table("ranker");
     if (!group)
-        throw std::runtime_error{"\"ranker\" group needed in config file!"};
+        throw std::runtime_error{"\"ranker\" group needed in config"};
     auto ranker = index::make_ranker(*group);
 
+    // Get the config group with options specific to this executable.
+    auto query_group = config->get_table("query-runner");
+    if (!query_group)
+        throw std::runtime_error{"\"query-runner\" group needed in config"};
+
     // Get the path to the file containing queries
-    auto query_path = config->get_as<std::string>("query-path");
+    auto query_path = query_group->get_as<std::string>("query-path");
     if (!query_path)
         throw std::runtime_error{
             "config file needs a \"query-path\" parameter"};
+    if (!meta::filesystem::file_exists(*query_path))
+        throw std::runtime_error{"query path does not exist: " + *query_path};
     std::ifstream queries{*query_path};
 
+    // Read the rest of the options for this executable.
+    auto trec_format = query_group->get_as<bool>("trec-format").value_or(false);
+    auto max_results = static_cast<uint64_t>(
+        query_group->get_as<int64_t>("max-results").value_or(10));
+    auto q_id = static_cast<uint64_t>(
+        query_group->get_as<int64_t>("query-id-start").value_or(1));
+
+    // create the IR evaluation scorer if necessary
     std::unique_ptr<index::ir_eval> eval;
     try
     {
-        eval = make_unique<index::ir_eval>(*config);
+        if (!trec_format)
+            eval = make_unique<index::ir_eval>(*query_group);
     }
     catch (index::ir_eval_exception& ex)
     {
@@ -67,52 +123,41 @@ int main(int argc, char* argv[])
 
     std::string content;
     auto elapsed_seconds = common::time([&]() {
-        size_t i = 0;
         while (std::getline(queries, content))
         {
             corpus::document query{doc_id{0}};
             query.content(content);
-            std::cout << std::string(80, '=') << std::endl;
-            std::cout << "Query " << ++i << ": \"" << content << "\""
-                      << std::endl;
-            std::cout << std::string(80, '-') << std::endl;
-
-            // Use the ranker to score the query over the index.
-            auto ranking = ranker->score(*idx, query);
-            auto result_num = 1;
+            if (!trec_format)
+            {
+                std::cout << std::string(80, '=') << std::endl;
+                std::cout << "Query " << q_id << ": \"" << content << "\""
+                          << std::endl;
+                std::cout << std::string(80, '-') << std::endl;
+            }
+            auto ranking = ranker->score(*idx, query, max_results);
+            uint64_t result_num = 1;
             for (auto& result : ranking)
             {
-                std::string path{idx->doc_path(result.d_id)};
-                auto output = printing::make_bold(std::to_string(result_num)
-                                                  + ". " + path)
-                              + " (score = " + std::to_string(result.score)
-                              + ", docid = " + std::to_string(result.d_id)
-                              + ")";
-                std::cout << output << std::endl;
-                auto mdata = idx->metadata(result.d_id);
-                if (auto content = mdata.get<std::string>("content"))
-                {
-                    auto len
-                        = std::min(std::string::size_type{77}, content->size());
-                    std::cout << content->substr(0, len) << "..." << std::endl
-                              << std::endl;
-                }
-                if (result_num++ == 10)
+                if (trec_format)
+                    print_trec(idx, result, result_num, q_id);
+                else
+                    print_results(idx, result, result_num);
+                if (result_num++ == max_results)
                     break;
             }
-            if (eval)
-                eval->print_stats(ranking, query_id{i - 1});
-            std::cout << std::endl;
+            if (!trec_format && eval)
+                eval->print_stats(ranking, query_id{q_id});
+            ++q_id;
         }
     });
 
-    if (eval)
+    if (!trec_format && eval)
     {
         std::cout << printing::make_bold("  MAP: ") << eval->map() << std::endl;
         std::cout << printing::make_bold(" gMAP: ") << eval->gmap()
                   << std::endl;
         std::cout << std::endl;
     }
-    std::cout << "Elapsed time: " << elapsed_seconds.count() << "ms"
+    std::cerr << "Elapsed time: " << elapsed_seconds.count() << "ms"
               << std::endl;
 }
