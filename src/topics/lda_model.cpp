12c08cd5b54c7b4e0bd12f13492cb4832c93a56f
Matt Kelly <mkelly3890@outlook.com>
2017-07-06 20:00:16 -0500

    Update save functions to take a stream

--- src/topics/lda_model.cpp
+++ src/topics/lda_model.cpp
@@ -20,27 +20,23 @@ lda_model::lda_model(std::shared_ptr<index::forward_index> idx,
     /* nothing */
 }
 
-void lda_model::save_doc_topic_distributions(const std::string& filename) const
+void lda_model::save_doc_topic_distributions(std::ostream& stream) const
 {
-    std::ofstream file{filename, std::ios::binary};
-
-    io::packed::write(file, idx_->docs().size());
-    io::packed::write(file, num_topics_);
+    io::packed::write(stream, idx_->docs().size());
+    io::packed::write(stream, num_topics_);
 
     for (const auto& d_id : idx_->docs())
     {
         auto dist = topic_distrbution(d_id);
         for (topic_id j{0}; j < num_topics_; ++j)
         {
-            io::packed::write(file, dist.counts(j));
+            io::packed::write(stream, dist.counts(j));
         }
     }
 }
 
-void lda_model::save_topic_term_distributions(const std::string& filename) const
+void lda_model::save_topic_term_distributions(std::ostream& stream) const
 {
-    std::ofstream file{filename, std::ios::binary};
-
     // first, compute the denominators for each term's normalized score
     std::vector<double> denoms;
     denoms.reserve(idx_->unique_terms());
@@ -53,8 +49,8 @@ void lda_model::save_topic_term_distributions(const std::string& filename) const
         denoms.push_back(denom);
     }
 
-    io::packed::write(file, num_topics_);
-    io::packed::write(file, idx_->unique_terms());
+    io::packed::write(stream, num_topics_);
+    io::packed::write(stream, idx_->unique_terms());
 
     // then, calculate and save each term's score
     for (topic_id j{0}; j < num_topics_; ++j)
@@ -63,15 +59,18 @@ void lda_model::save_topic_term_distributions(const std::string& filename) const
         {
             double prob = compute_term_topic_probability(t_id, j);
             double norm_prob = prob * std::log(prob / denoms[t_id]);
-            io::packed::write(file, norm_prob);
+            io::packed::write(stream, norm_prob);
         }
     }
 }
 
 void lda_model::save(const std::string& prefix) const
 {
-    save_doc_topic_distributions(prefix + ".theta");
-    save_topic_term_distributions(prefix + ".phi");
+    std::ofstream theta_file{prefix + ".theta", std::ios::binary};
+    std::ofstream phi_file{prefix + ".phi", std::ios::binary};
+
+    save_doc_topic_distributions(theta_file);
+    save_topic_term_distributions(phi_file);
 }
 
 uint64_t lda_model::num_topics() const
