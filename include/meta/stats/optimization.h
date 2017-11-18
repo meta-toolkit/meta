1aa70324bc96494801533d7ee880424d4169ea10
Aleksey <alex2304el@gmail.com>
2017-11-18 17:52:22 +0300

    + added optimization.h and .gitignore updated

---
+++ include/meta/stats/optimization.h
@@ -0,0 +1,25 @@
+#ifndef OPTIMIZATION_H
+#define OPTIMIZATION_H
+
+#include "meta/embeddings/word_embeddings.h"
+
+using namespace meta::embeddings;
+
+namespace meta
+{
+namespace stats
+{
+namespace opt
+{
+
+// first method
+double minka_fpi(word_embeddings model){
+    for (auto term_it: model.vocab()){
+
+    }
+}
+
+}
+}
+}
+#endif // OPTIMIZATION_H
