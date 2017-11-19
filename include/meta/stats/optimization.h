5e3e23c4623805352a357e4caca36b8e64cbe971
Aleksey <alex2304el@gmail.com>
2017-11-19 13:02:09 +0300

    [opt] dirichlet_optimizer class, digamma function

--- include/meta/stats/optimization.h
+++ include/meta/stats/optimization.h
@@ -2,8 +2,11 @@
 #define OPTIMIZATION_H
 
 #include "meta/embeddings/word_embeddings.h"
+#include "meta/stats/statistics.h"
 
+using namespace meta::stats;
 using namespace meta::embeddings;
+using namespace meta::util;
 
 namespace meta
 {
@@ -12,14 +15,56 @@ namespace stats
 namespace opt
 {
 
-// first method
-double minka_fpi(word_embeddings model){
-    for (auto term_it: model.vocab()){
+aligned_vector<std::string> get_docs_voc(aligned_vector<word_embeddings> docs_models){
+    aligned_vector<std::string> docs_voc();
 
+    for (auto m_iter: docs_models){
+        // todo
     }
+
+    return docs_voc;
 }
 
-}
-}
-}
+class dirichlet_optimizer{
+public:
+    dirichlet_optimizer(aligned_vector<word_embeddings> docs_models, int alpha=1){
+        this->docs_models_ = docs_models;
+        this->default_alpha_ = alpha;
+
+        this->docs_voc_ = get_docs_voc(docs_models);
+    }
+
+    double minka_fpi(){
+        double alpha = default_alpha_;
+
+        double nom, denom,
+                alpha_m,
+                alpha_dig, alpha_m_dig,
+                all_words_count, k_words_count;
+
+        // digamma(x)
+        for (std::string word: voc){
+            // todo
+        }
+    }
+
+    double minka_newton(){
+        // todo
+    }
+
+    double minka_lou(){
+        // todo
+    }
+
+private:
+    double minka_fpi_iters();
+    double minka_newton_iters();
+    double minka_lou_iters();
+
+    aligned_vector<word_embeddings> docs_models_;
+    aligned_vector<std::string> docs_voc_;
+
+    double default_alpha_;
+};
+
 #endif // OPTIMIZATION_H
--- include/meta/stats/statistics.h
+++ include/meta/stats/statistics.h
@@ -19,6 +19,107 @@ namespace meta
 namespace stats
 {
 
+#ifndef M_PIl
+/** The constant Pi in high precision */
+#define M_PIl 3.1415926535897932384626433832795029L
+#endif
+#ifndef M_GAMMAl
+/** Euler's constant in high precision */
+#define M_GAMMAl 0.5772156649015328606065120900824024L
+#endif
+#ifndef M_LN2l
+/** the natural logarithm of 2 in high precision */
+#define M_LN2l 0.6931471805599453094172321214581766L
+#endif
+
+/** The digamma function in long double precision.
+* @param x the real value of the argument
+* @return the value of the digamma (psi) function at that point
+* @author Richard J. Mathar
+* @since 2005-11-24
+*/
+long double digamma(long double x)
+{
+    /* force into the interval 1..3 */
+    if( x < 0.0L )
+        return digamma(1.0L-x)+M_PIl/tanl(M_PIl*(1.0L-x)) ;	/* reflection formula */
+    else if( x < 1.0L )
+        return digamma(1.0L+x)-1.0L/x ;
+    else if ( x == 1.0L)
+        return -M_GAMMAl ;
+    else if ( x == 2.0L)
+        return 1.0L-M_GAMMAl ;
+    else if ( x == 3.0L)
+        return 1.5L-M_GAMMAl ;
+    else if ( x > 3.0L)
+        /* duplication formula */
+        return 0.5L*(digamma(x/2.0L)+digamma((x+1.0L)/2.0L))+M_LN2l ;
+    else
+    {
+        /* Just for your information, the following lines contain
+        * the Maple source code to re-generate the table that is
+        * eventually becoming the Kncoe[] array below
+        * interface(prettyprint=0) :
+        * Digits := 63 :
+        * r := 0 :
+        *
+        * for l from 1 to 60 do
+        * 	d := binomial(-1/2,l) :
+        * 	r := r+d*(-1)^l*(Zeta(2*l+1) -1) ;
+        * 	evalf(r) ;
+        * 	print(%,evalf(1+Psi(1)-r)) ;
+        *o d :
+        *
+        * for N from 1 to 28 do
+        * 	r := 0 :
+        * 	n := N-1 :
+        *
+        *	for l from iquo(n+3,2) to 70 do
+        *		d := 0 :
+        *		for s from 0 to n+1 do
+        *		 d := d+(-1)^s*binomial(n+1,s)*binomial((s-1)/2,l) :
+        *		od :
+        *		if 2*l-n > 1 then
+        *		r := r+d*(-1)^l*(Zeta(2*l-n) -1) :
+        *		fi :
+        *	od :
+        *	print(evalf((-1)^n*2*r)) ;
+        *od :
+        *quit :
+        */
+        static long double Kncoe[] = { .30459198558715155634315638246624251L,
+        .72037977439182833573548891941219706L, -.12454959243861367729528855995001087L,
+        .27769457331927827002810119567456810e-1L, -.67762371439822456447373550186163070e-2L,
+        .17238755142247705209823876688592170e-2L, -.44817699064252933515310345718960928e-3L,
+        .11793660000155572716272710617753373e-3L, -.31253894280980134452125172274246963e-4L,
+        .83173997012173283398932708991137488e-5L, -.22191427643780045431149221890172210e-5L,
+        .59302266729329346291029599913617915e-6L, -.15863051191470655433559920279603632e-6L,
+        .42459203983193603241777510648681429e-7L, -.11369129616951114238848106591780146e-7L,
+        .304502217295931698401459168423403510e-8L, -.81568455080753152802915013641723686e-9L,
+        .21852324749975455125936715817306383e-9L, -.58546491441689515680751900276454407e-10L,
+        .15686348450871204869813586459513648e-10L, -.42029496273143231373796179302482033e-11L,
+        .11261435719264907097227520956710754e-11L, -.30174353636860279765375177200637590e-12L,
+        .80850955256389526647406571868193768e-13L, -.21663779809421233144009565199997351e-13L,
+        .58047634271339391495076374966835526e-14L, -.15553767189204733561108869588173845e-14L,
+        .41676108598040807753707828039353330e-15L, -.11167065064221317094734023242188463e-15L } ;
+
+        register long double Tn_1 = 1.0L ;	/* T_{n-1}(x), started at n=1 */
+        register long double Tn = x-2.0L ;	/* T_{n}(x) , started at n=1 */
+        register long double resul = Kncoe[0] + Kncoe[1]*Tn ;
+
+        x -= 2.0L ;
+
+        for(int n = 2 ; n < sizeof(Kncoe)/sizeof(long double) ;n++)
+        {
+            const long double Tn1 = 2.0L * x * Tn - Tn_1 ;	/* Chebyshev recursion, Eq. 22.7.4 Abramowitz-Stegun */
+            resul += Kncoe[n]*Tn1 ;
+            Tn_1 = Tn ;
+            Tn = Tn1 ;
+        }
+        return resul ;
+    }
+}
+
 /**
  * Computation for \f$E_d[f(x)]\f$ where \f$d\f$ is specified by the
  * `dist` parameter and \f$f(x)\f$ is the `fun` parameter.
