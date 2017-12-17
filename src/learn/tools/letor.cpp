bae0c78f517103cc12047ef66c254e392bc7a937
hksskh <hksskh@126.com>
2017-12-17 08:07:31 -0600

    fix docid

--- src/learn/tools/letor.cpp
+++ src/learn/tools/letor.cpp
@@ -483,9 +483,7 @@ void read_data(DATA_TYPE data_type, string data_dir, vector<string> *qids,
             ssval >> feature_val;
             features[term_id{feature_id - 1}] = feature_val;
         }
-        iss>>tmp_str;
-        iss>>tmp_str;
-        iss>>docid;
+
         if (data_type != TRAINING) {
             if (docids->find(qid) == docids->end()) {
                 (*docids)[qid] = unordered_map<int, vector<string> >();
@@ -498,8 +496,7 @@ void read_data(DATA_TYPE data_type, string data_dir, vector<string> *qids,
             }
             vector<string> &label_docids = query_docids[label];
 
-            //docid = std::to_string((*qid_docids)[qid]++);
-            cout << docid << endl;
+            docid = qid + std::to_string((*qid_docids)[qid]++);
             label_docids.push_back(docid);
             if (relevance_map->find(qid) == relevance_map->end()) {
                 (*relevance_map)[qid] = unordered_map<string, int>();
